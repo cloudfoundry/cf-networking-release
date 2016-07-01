871e9c16604297b8abbe9a471396dae82e702135
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-07-01 15:23:09 -0700

    Fix race condition in netman-agent acceptance tests
    [#123261441]
    
    Signed-off-by: Jay Dunkelberger <ldunkelberger@pivotal.io>

--- src/netman-agent/acceptance/acceptance_test.go
+++ src/netman-agent/acceptance/acceptance_test.go
@@ -10,6 +10,7 @@ import (
 	"netman-agent/config"
 	"os/exec"
 	"strings"
+	"sync"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -17,35 +18,49 @@ import (
 	"github.com/onsi/gomega/gexec"
 )
 
-var mockPolicyServerResponseCode int = 200
-
-func createMockPolicyServer() *httptest.Server {
-	var serverCallCount = 0
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/networking/v0/internal/policies" {
-			serverCallCount += 1
-			w.WriteHeader(mockPolicyServerResponseCode)
-			w.Write([]byte(fmt.Sprintf(`{ "policies": [{"source": {"id": "app-%d"}, "destination": { "id": "other-app", "port": 8080, "protocol": "tcp"}}]}`, serverCallCount)))
-			return
-		}
-		w.WriteHeader(http.StatusTeapot)
-		w.Write([]byte(fmt.Sprintf(`you asked for a path that we do not mock %s`, r.URL.Path)))
-	}))
-}
-
 var _ = Describe("Acceptance", func() {
 	var (
-		session          *gexec.Session
-		conf             config.Config
-		mockPolicyServer *httptest.Server
-		address          string
+		session                      *gexec.Session
+		conf                         config.Config
+		mockPolicyServer             *httptest.Server
+		address                      string
+		mockPolicyServerResponseCode int
+		mockServerControl            *sync.Mutex
 	)
+
+	// without a lock around it, we get race conditions when changing the policy server response code
+	setResponseCode := func(code int) {
+		mockServerControl.Lock()
+		defer mockServerControl.Unlock()
+		mockPolicyServerResponseCode = code
+	}
+	getResponseCode := func() int {
+		mockServerControl.Lock()
+		defer mockServerControl.Unlock()
+		return mockPolicyServerResponseCode
+	}
+
+	createMockPolicyServer := func() *httptest.Server {
+		var serverCallCount = 0
+		return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/networking/v0/internal/policies" {
+				serverCallCount += 1
+				w.WriteHeader(getResponseCode())
+				w.Write([]byte(fmt.Sprintf(`{ "policies": [{"source": {"id": "app-%d"}, "destination": { "id": "other-app", "port": 8080, "protocol": "tcp"}}]}`, serverCallCount)))
+				return
+			}
+			w.WriteHeader(http.StatusTeapot)
+			w.Write([]byte(fmt.Sprintf(`you asked for a path that we do not mock %s`, r.URL.Path)))
+		}))
+	}
 
 	var serverIsAvailable = func() error {
 		return VerifyTCPConnection(address)
 	}
 
 	BeforeEach(func() {
+		mockServerControl = &sync.Mutex{}
+		setResponseCode(200)
 		mockPolicyServer = createMockPolicyServer()
 		listenPort := 6666 + GinkgoParallelNode() + rand.Intn(5000)
 		address = fmt.Sprintf("127.0.0.1:%d", listenPort)
@@ -108,7 +123,7 @@ var _ = Describe("Acceptance", func() {
 
 		Context("if the policy server responds with an error status code", func() {
 			BeforeEach(func() {
-				mockPolicyServerResponseCode = 409
+				setResponseCode(409)
 			})
 			It("should log", func() {
 				Eventually(session.Out, DEFAULT_TIMEOUT).Should(gbytes.Say(`policy-client.http-client.*app-.*409`))
