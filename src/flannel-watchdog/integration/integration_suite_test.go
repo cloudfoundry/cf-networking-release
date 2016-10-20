ccf4a115ab23e47292a468575b0d4f0e0d3cc8ab
Genevieve LEsperance <glesperance@pivotal.io>
2016-10-19 22:06:36 -0700

    Flannel watchdog integration tests build binary once
    [#132806187]

--- src/flannel-watchdog/integration/integration_suite_test.go
+++ src/flannel-watchdog/integration/integration_suite_test.go
@@ -21,17 +21,20 @@ func TestIntegration(t *testing.T) {
 	RunSpecs(t, "Integration Suite")
 }
 
-var _ = BeforeSuite(func() {
-	// only run on node 1
+var _ = SynchronizedBeforeSuite(func() []byte {
 	fmt.Fprintf(GinkgoWriter, "building binary...")
 	var err error
 	watchdogBinaryPath, err = gexec.Build("flannel-watchdog/cmd/flannel-watchdog", "-race")
 	fmt.Fprintf(GinkgoWriter, "done")
 	Expect(err).NotTo(HaveOccurred())
 
+	return []byte(watchdogBinaryPath)
+}, func(data []byte) {
+	watchdogBinaryPath = string(data)
+
 	rand.Seed(ginkgoConfig.GinkgoConfig.RandomSeed + int64(GinkgoParallelNode()))
 })
 
-var _ = AfterSuite(func() {
+var _ = SynchronizedAfterSuite(func() {}, func() {
 	gexec.CleanupBuildArtifacts()
 })
