9ef8a42fedb64303b9b492d3c8a3ec0a16224892
Konstantinos Karampogias <konstantinos.karampogias@swisscom.com>
2016-11-11 13:44:45 -0800

    Backfill unit-tests for vxlan-policy-agent config
    [#133694695]
    
    Signed-off-by: Gabe Rosenhouse <grosenhouse@pivotal.io>

--- src/vxlan-policy-agent/config/config.go
+++ src/vxlan-policy-agent/config/config.go
@@ -1,13 +1,45 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/validator.v2"
+)
+
 type VxlanPolicyAgent struct {
-	PollInterval      int    `json:"poll_interval"`
-	Datastore         string `json:"cni_datastore_path"`
-	PolicyServerURL   string `json:"policy_server_url"`
-	VNI               int    `json:"vni"`
-	FlannelSubnetFile string `json:"flannel_subnet_file"`
-	MetronAddress     string `json:"metron_address"`
-	ServerCACertFile  string `json:"ca_cert_file"`
-	ClientCertFile    string `json:"client_cert_file"`
-	ClientKeyFile     string `json:"client_key_file"`
+	PollInterval      int    `json:"poll_interval" validate:"nonzero"`
+	Datastore         string `json:"cni_datastore_path" validate:"nonzero"`
+	PolicyServerURL   string `json:"policy_server_url" validate:"nonzero"`
+	VNI               int    `json:"vni" validate:"nonzero"`
+	FlannelSubnetFile string `json:"flannel_subnet_file" validate:"nonzero"`
+	MetronAddress     string `json:"metron_address" validate:"nonzero"`
+	ServerCACertFile  string `json:"ca_cert_file" validate:"nonzero"`
+	ClientCertFile    string `json:"client_cert_file" validate:"nonzero"`
+	ClientKeyFile     string `json:"client_key_file" validate:"nonzero"`
+}
+
+func New(configFilePath string) (*VxlanPolicyAgent, error) {
+	cfg := &VxlanPolicyAgent{}
+	if _, err := os.Stat(configFilePath); err != nil {
+		return cfg, fmt.Errorf("file does not exist: %s", err)
+	}
+
+	configBytes, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		return cfg, fmt.Errorf("reading config file: %s", err)
+	}
+
+	err = json.Unmarshal(configBytes, &cfg)
+	if err != nil {
+		return cfg, fmt.Errorf("parsing config (%s): %s", configFilePath, err)
+	}
+
+	if errs := validator.Validate(cfg); errs != nil {
+		return cfg, fmt.Errorf("invalid config: %s", errs)
+	}
+
+	return cfg, nil
 }
---
+++ src/vxlan-policy-agent/config/config_test.go
@@ -0,0 +1,106 @@
+package config_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"vxlan-policy-agent/config"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Config", func() {
+	Describe("New", func() {
+		var (
+			file *os.File
+			err  error
+		)
+
+		BeforeEach(func() {
+			file, err = ioutil.TempFile(os.TempDir(), "config-")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		Context("when config file is valid", func() {
+			It("returns the config", func() {
+				file.WriteString(`{
+					"poll_interval": 1234,
+					"cni_datastore_path": "/some/datastore/path",
+					"policy_server_url": "https://some-url:1234",
+					"vni": 42,
+					"flannel_subnet_file": "/some/subnet/file",
+					"metron_address": "http://1.2.3.4:1234",
+					"ca_cert_file": "/some/ca/file",
+					"client_cert_file": "/some/client/cert/file",
+					"client_key_file": "/some/client/key/file"
+				}`)
+				c, err := config.New(file.Name())
+				Expect(err).NotTo(HaveOccurred())
+				Expect(c.PollInterval).To(Equal(1234))
+				Expect(c.Datastore).To(Equal("/some/datastore/path"))
+				Expect(c.PolicyServerURL).To(Equal("https://some-url:1234"))
+				Expect(c.VNI).To(Equal(42))
+				Expect(c.FlannelSubnetFile).To(Equal("/some/subnet/file"))
+				Expect(c.MetronAddress).To(Equal("http://1.2.3.4:1234"))
+				Expect(c.ServerCACertFile).To(Equal("/some/ca/file"))
+				Expect(c.ClientCertFile).To(Equal("/some/client/cert/file"))
+				Expect(c.ClientKeyFile).To(Equal("/some/client/key/file"))
+			})
+		})
+
+		Context("when config file path does not exist", func() {
+			It("returns the error", func() {
+				_, err := config.New("not-exists")
+				Expect(err).To(MatchError(ContainSubstring("file does not exist:")))
+			})
+		})
+
+		Context("when config file is bad format", func() {
+			It("returns the error", func() {
+				file.WriteString("bad-format")
+				_, err = config.New(file.Name())
+				Expect(err).To(MatchError(ContainSubstring("parsing config")))
+			})
+		})
+
+		Context("when config file contents blank", func() {
+			It("returns the error", func() {
+				_, err = config.New(file.Name())
+				Expect(err).To(MatchError(ContainSubstring("parsing config")))
+			})
+		})
+
+		DescribeTable("when config file is missing a member",
+			func(missingFlag, errorMsg string) {
+				allData := map[string]interface{}{
+					"poll_interval":      1234,
+					"cni_datastore_path": "/some/datastore/path",
+					"policy_server_url":  "https://some-url:1234",
+					"vni":                42,
+					"flannel_subnet_file": "/some/subnet/file",
+					"metron_address":      "http://1.2.3.4:1234",
+					"ca_cert_file":        "/some/ca/file",
+					"client_cert_file":    "/some/client/cert/file",
+					"client_key_file":     "/some/client/key/file",
+				}
+				delete(allData, missingFlag)
+				Expect(json.NewEncoder(file).Encode(allData)).To(Succeed())
+
+				_, err = config.New(file.Name())
+				Expect(err).To(MatchError(fmt.Sprintf("invalid config: %s", errorMsg)))
+			},
+			Entry("missing poll interval", "poll_interval", "PollInterval: zero value"),
+			Entry("missing datastore path", "cni_datastore_path", "Datastore: zero value"),
+			Entry("missing policy server url", "policy_server_url", "PolicyServerURL: zero value"),
+			Entry("missing vni", "vni", "VNI: zero value"),
+			Entry("missing flannel subnet file", "flannel_subnet_file", "FlannelSubnetFile: zero value"),
+			Entry("missing metron address", "metron_address", "MetronAddress: zero value"),
+			Entry("missing ca cert", "ca_cert_file", "ServerCACertFile: zero value"),
+			Entry("missing client cert file", "client_cert_file", "ClientCertFile: zero value"),
+			Entry("missing client key file", "client_key_file", "ClientKeyFile: zero value"),
+		)
+	})
+})
