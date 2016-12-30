6724d2f2cc3767f46c02e4c7f8191cc852e13728
Gabe Rosenhouse <grosenhouse@pivotal.io>
2016-12-30 07:28:48 -0800

    WIP - adds failing acceptance test of space dev workflow
    uses branch of warrant library that supports group membership operations
    
    Submodule src/github.com/pivotal-cf-experimental/warrant e60c40d..021d5c9:
      > Group membership: list, add, remove
      > The token should inherit authorities
      > goimports
      > Remove use of symmetric keys and use RSA Public/Private Key for the test server
    
    [#131741793]
    
    Signed-off-by: Genevieve LEsperance <glesperance@pivotal.io>

---
+++ src/netman-cf-acceptance/space_developer_test.go
@@ -0,0 +1,134 @@
+package acceptance_test
+
+import (
+	"crypto/tls"
+	"fmt"
+	"lib/models"
+	"lib/policy_client"
+	"math/rand"
+	"net/http"
+	"strings"
+
+	"code.cloudfoundry.org/lager/lagertest"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+	"github.com/pivotal-cf-experimental/warrant"
+)
+
+var _ = Describe("external connectivity", func() {
+	var (
+		appA       string
+		appB       string
+		orgName    string
+		spaceNameA string
+		spaceNameB string
+		w          warrant.Warrant
+
+		policyClient *policy_client.ExternalClient
+	)
+
+	BeforeEach(func() {
+		policyClient = policy_client.NewExternal(lagertest.NewTestLogger("test"),
+			&http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{
+						InsecureSkipVerify: true,
+					},
+				},
+			},
+			fmt.Sprintf("https://%s", config.ApiEndpoint),
+		)
+
+		appA = fmt.Sprintf("appA-%d", rand.Int31())
+		appB = fmt.Sprintf("appB-%d", rand.Int31())
+
+		AuthAsAdmin()
+
+		orgName = "test-org"
+		Expect(cf.Cf("create-org", orgName).Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("target", "-o", orgName).Wait(Timeout_Push)).To(gexec.Exit(0))
+
+		spaceNameA = "test-space-A"
+		Expect(cf.Cf("create-space", spaceNameA).Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("target", "-o", orgName, "-s", spaceNameA).Wait(Timeout_Push)).To(gexec.Exit(0))
+
+		// pushProxy(appA)
+
+		spaceNameB = "test-space-B"
+		Expect(cf.Cf("create-space", spaceNameB).Wait(Timeout_Push)).To(gexec.Exit(0))
+		Expect(cf.Cf("target", "-o", orgName, "-s", spaceNameB).Wait(Timeout_Push)).To(gexec.Exit(0))
+
+		// pushProxy(appB)
+
+		w = warrant.New(warrant.Config{
+			Host:          fmt.Sprintf("https://uaa.%s", config.AppsDomain), // TODO(gabe): should be system domain
+			SkipVerifySSL: true,
+		})
+
+		// UAA group and user assignment stuff
+		uaaAdminClientToken, err := w.Clients.GetToken("admin", "admin-secret")
+		Expect(err).NotTo(HaveOccurred())
+
+		user := ensureUserExists(w, "space-developer", "password", uaaAdminClientToken)
+		group := ensureGroupExists(w, "network.write", uaaAdminClientToken)
+
+		err = w.Groups.AddMember(group.ID, user.ID, uaaAdminClientToken)
+		Expect(err).To(Or(BeNil(), BeAssignableToTypeOf(warrant.DuplicateResourceError{})))
+	})
+
+	AfterEach(func() {
+		Expect(cf.Cf("delete-org", orgName, "-f").Wait(Timeout_Push)).To(gexec.Exit(0))
+	})
+
+	FDescribe("space developer with network.write scope", func() {
+		It("can create network policies in spaces they have access to", func(done Done) {
+			By("something")
+
+			Expect(cf.Cf("set-space-role", "space-developer", orgName, spaceNameA, "SpaceDeveloper").Wait(Timeout_Push)).To(gexec.Exit(0))
+			Expect(cf.Cf("set-space-role", "space-developer", orgName, spaceNameB, "SpaceDeveloper").Wait(Timeout_Push)).To(gexec.Exit(0))
+
+			// log in as user
+			Expect(cf.Cf("auth", "space-developer", "password").Wait(Timeout_Push)).To(gexec.Exit(0))
+			getTokenSession := cf.Cf("oauth-token")
+			Expect(getTokenSession.Wait(Timeout_Push)).To(gexec.Exit(0))
+			spaceDevUserToken := strings.TrimSpace(string(getTokenSession.Out.Contents()))
+			err := policyClient.AddPolicies(spaceDevUserToken, []models.Policy{
+				models.Policy{
+					Source: models.Source{
+						ID: "appAGuidgoes here",
+					},
+					Destination: models.Destination{
+						ID:       "appB guid goes here",
+						Port:     1234,
+						Protocol: "tcp",
+					},
+				},
+			})
+			Expect(err).NotTo(HaveOccurred())
+			close(done)
+		}, 60 /* <-- overall spec timeout in seconds */)
+	})
+})
+
+func ensureGroupExists(client warrant.Warrant, name, token string) warrant.Group {
+	_, err := client.Groups.Create(name, token)
+	Expect(err).To(Or(BeNil(), BeAssignableToTypeOf(warrant.DuplicateResourceError{})))
+
+	groups, err := client.Groups.List(warrant.Query{Filter: fmt.Sprintf(`displayName eq %q`, name)}, token)
+	Expect(err).NotTo(HaveOccurred())
+
+	return groups[0]
+}
+
+func ensureUserExists(warrantClient warrant.Warrant, username, password, token string) warrant.User {
+	createUserSession := cf.Cf("create-user", username, password)
+	Expect(createUserSession.Wait(Timeout_Push)).To(gexec.Exit(0))
+
+	users, err := warrantClient.Users.List(warrant.Query{Filter: fmt.Sprintf(`userName eq %q`, username)}, token)
+	Expect(err).NotTo(HaveOccurred())
+
+	return users[0]
+}
