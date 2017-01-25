45030714d133bb2e574bcdd5aa68e9b33e670caa
Genevieve LEsperance <glesperance@pivotal.io>
2017-01-25 15:54:46 -0800

    Refactor big asg acceptance test
    [#138208269]

--- src/netman-cf-acceptance/big_asg_test.go
+++ src/netman-cf-acceptance/big_asg_test.go
@@ -16,7 +16,6 @@ import (
 
 var _ = Describe("policy cleanup", func() {
 	var (
-		appA, appB  string
 		orgName     string
 		spaceName   string
 		cli         *cf_cli_adapter.Adapter
@@ -24,9 +23,6 @@ var _ = Describe("policy cleanup", func() {
 	)
 
 	BeforeEach(func() {
-		appA = fmt.Sprintf("appA-%d", rand.Int31())
-		appB = fmt.Sprintf("appB-%d", rand.Int31())
-
 		cli = &cf_cli_adapter.Adapter{
 			CfCliPath: "cf",
 		}
@@ -39,7 +35,6 @@ var _ = Describe("policy cleanup", func() {
 		spaceName = "asg-space"
 		Expect(cli.CreateSpace(spaceName)).To(Succeed())
 		Expect(cli.TargetSpace(spaceName)).To(Succeed())
-
 	})
 
 	AfterEach(func() {
@@ -48,29 +43,38 @@ var _ = Describe("policy cleanup", func() {
 		os.Remove(ASGFilepath)
 	})
 
-	Describe("Pushing app with and without ASG", func() {
+	Describe("Pushing app with and without a large application security group (ASG)", func() {
 		It("should not give large time difference", func() {
-			By("pushing an app")
-			start := time.Now()
-			pushProxy(appB)
-			duration := time.Since(start)
+			var (
+				durationWithoutASG time.Duration
+				durationWithASG    time.Duration
+			)
 
-			By("creating large ASG")
-			asg := testsupport.BuildASG(1000)
-			var err error
-			ASGFilepath, err = testsupport.CreateASGFile(asg)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(cli.CreateSecurityGroup("big-asg", ASGFilepath)).To(Succeed())
-			By("binding ASG to the space")
-			Expect(cli.BindSecurityGroup("big-asg", orgName, spaceName)).To(Succeed())
+			By("pushing an app", func() {
+				start := time.Now()
+				appB := fmt.Sprintf("appB-%d", rand.Int31())
+				pushProxy(appB)
+				durationWithoutASG = time.Since(start)
+			})
 
-			By("pushing an app")
-			start = time.Now()
-			pushProxy(appA)
-			durationWithASG := time.Since(start)
+			By("creating a large ASG", func() {
+				asg := testsupport.BuildASG(1000)
+				var err error
+				ASGFilepath, err = testsupport.CreateASGFile(asg)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(cli.CreateSecurityGroup("big-asg", ASGFilepath)).To(Succeed())
+				Expect(cli.BindSecurityGroup("big-asg", orgName, spaceName)).To(Succeed())
+			})
+
+			By("pushing another app", func() {
+				start := time.Now()
+				appA := fmt.Sprintf("appA-%d", rand.Int31())
+				pushProxy(appA)
+				durationWithASG = time.Since(start)
+			})
 
 			fmt.Println("##############################################")
-			fmt.Println("push app without ASG took:", duration)
+			fmt.Println("push app without ASG took:", durationWithoutASG)
 			fmt.Println("push app with big ASG took:", durationWithASG)
 			fmt.Println("##############################################")
 		})
