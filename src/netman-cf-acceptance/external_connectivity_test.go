903cd1173615dad6306928cc0a8da7cbb0df5bc5
Genevieve LEsperance <glesperance@pivotal.io>
2017-01-25 16:09:42 -0800

    Refactor external connectivity acceptance tests
    [#138208269]

--- src/netman-cf-acceptance/external_connectivity_test.go
+++ src/netman-cf-acceptance/external_connectivity_test.go
@@ -17,7 +17,6 @@ var _ = Describe("external connectivity", func() {
 	var (
 		appA                          string
 		orgName                       string
-		spaceName                     string
 		appRoute                      string
 		prefix                        string
 		originalRunningSecurityGroups []string
@@ -33,7 +32,7 @@ var _ = Describe("external connectivity", func() {
 		Expect(cf.Cf("create-org", orgName).Wait(Timeout_Push)).To(gexec.Exit(0))
 		Expect(cf.Cf("target", "-o", orgName).Wait(Timeout_Push)).To(gexec.Exit(0))
 
-		spaceName = prefix + "space"
+		spaceName := prefix + "space"
 		Expect(cf.Cf("create-space", spaceName).Wait(Timeout_Push)).To(gexec.Exit(0))
 		Expect(cf.Cf("target", "-o", orgName, "-s", spaceName).Wait(Timeout_Push)).To(gexec.Exit(0))
 
@@ -44,66 +43,71 @@ var _ = Describe("external connectivity", func() {
 		for _, sg := range allSecurityGroups {
 			Expect(cf.Cf("bind-running-security-group", sg).Wait(Timeout_Short)).To(gexec.Exit(0))
 		}
+
 		originalRunningSecurityGroups = getRunningSecurityGroups()
 	})
 
 	AfterEach(func() {
 		appReport(appA, Timeout_Short)
-
-		// clean up everything
 		Expect(cf.Cf("delete-org", orgName, "-f").Wait(Timeout_Push)).To(gexec.Exit(0))
 
-		By("adding back all the original security groups")
-		for _, sg := range originalRunningSecurityGroups {
-			Expect(cf.Cf("bind-running-security-group", sg).Wait(Timeout_Short)).To(gexec.Exit(0))
-		}
+		By("adding back all the original security groups", func() {
+			for _, sg := range originalRunningSecurityGroups {
+				Expect(cf.Cf("bind-running-security-group", sg).Wait(Timeout_Short)).To(gexec.Exit(0))
+			}
+		})
 	})
 
 	Describe("basic (legacy) network behavior for an app", func() {
 		It("is reachable from the router, and can reach the internet only if allowed", func(done Done) {
-			By("checking that the app is reachable via the router")
-			Consistently(func() bool {
-				resp, err := http.Get(appRoute)
-				Expect(err).NotTo(HaveOccurred())
-				defer resp.Body.Close()
-				Expect(resp.StatusCode).To(Equal(200))
-				respBytes, err := ioutil.ReadAll(resp.Body)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(respBytes).To(ContainSubstring(`{"ListenAddresses":[`))
-				return true
-			}, "10s", "1s").Should(BeTrue())
+			By("checking that the app is reachable via the router", func() {
+				Consistently(func() bool {
+					resp, err := http.Get(appRoute)
+					Expect(err).NotTo(HaveOccurred())
+					defer resp.Body.Close()
+					Expect(resp.StatusCode).To(Equal(200))
+					respBytes, err := ioutil.ReadAll(resp.Body)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(respBytes).To(ContainSubstring(`{"ListenAddresses":[`))
+					return true
+				}, "10s", "1s").Should(BeTrue())
+			})
 
-			By("checking that it can reach the internet")
-			Consistently(func() bool {
-				resp, err := http.Get(appRoute + "proxy/example.com")
-				Expect(err).NotTo(HaveOccurred())
-				defer resp.Body.Close()
-				Expect(resp.StatusCode).To(Equal(200))
-				respBytes, err := ioutil.ReadAll(resp.Body)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(respBytes).To(ContainSubstring("Example Domain"))
-				return true
-			}, "10s", "1s").Should(BeTrue())
+			By("checking that it can reach the internet", func() {
+				Consistently(func() bool {
+					resp, err := http.Get(appRoute + "proxy/example.com")
+					Expect(err).NotTo(HaveOccurred())
+					defer resp.Body.Close()
+					Expect(resp.StatusCode).To(Equal(200))
+					respBytes, err := ioutil.ReadAll(resp.Body)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(respBytes).To(ContainSubstring("Example Domain"))
+					return true
+				}, "10s", "1s").Should(BeTrue())
+			})
 
-			By("removing all the original security groups")
-			for _, sg := range originalRunningSecurityGroups {
-				Expect(cf.Cf("unbind-running-security-group", sg).Wait(Timeout_Short)).To(gexec.Exit(0))
-			}
+			By("removing all the original security groups", func() {
+				for _, sg := range originalRunningSecurityGroups {
+					Expect(cf.Cf("unbind-running-security-group", sg).Wait(Timeout_Short)).To(gexec.Exit(0))
+				}
+			})
 
-			By("restarting the app")
-			Expect(cf.Cf("restart", appA).Wait(Timeout_Push)).To(gexec.Exit(0))
+			By("restarting the app", func() {
+				Expect(cf.Cf("restart", appA).Wait(Timeout_Push)).To(gexec.Exit(0))
+			})
 
-			By("checking that the app cannot reach the internet")
-			Consistently(func() bool {
-				resp, err := http.Get(appRoute + "proxy/example.com")
-				Expect(err).NotTo(HaveOccurred())
-				defer resp.Body.Close()
-				Expect(resp.StatusCode).To(Equal(500))
-				respBytes, err := ioutil.ReadAll(resp.Body)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(respBytes).To(ContainSubstring("example.com"))
-				return true
-			}, "10s", "1s").Should(BeTrue())
+			By("checking that the app cannot reach the internet", func() {
+				Consistently(func() bool {
+					resp, err := http.Get(appRoute + "proxy/example.com")
+					Expect(err).NotTo(HaveOccurred())
+					defer resp.Body.Close()
+					Expect(resp.StatusCode).To(Equal(500))
+					respBytes, err := ioutil.ReadAll(resp.Body)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(respBytes).To(ContainSubstring("example.com"))
+					return true
+				}, "10s", "1s").Should(BeTrue())
+			})
 
 			close(done)
 		}, 60 /* <-- overall spec timeout in seconds */)
