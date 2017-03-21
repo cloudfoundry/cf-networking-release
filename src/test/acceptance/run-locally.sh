f20b0bcf2bdc617185156c94e42baa8f439da5cf
Angela Chin <achin@pivotal.io>
2017-03-20 18:00:04 -0700

    opsfile changes to support cf-deployment on bosh-lite
    [#141820895]
    
    Signed-off-by: Gabe Rosenhouse <grosenhouse@pivotal.io>

--- src/test/acceptance/run-locally.sh
+++ src/test/acceptance/run-locally.sh
@@ -8,12 +8,15 @@ cd $THIS_DIR
 export CONFIG=/tmp/test-config.json
 export APPS_DIR=../../example-apps
 
+VARS_STORE="$HOME/workspace/container-networking-deployments/environments/local/deployment-vars.yml"
+
+
 echo '
 {
   "api": "api.bosh-lite.com",
   "admin_user": "admin",
-  "admin_password": "admin",
-  "admin_secret": "admin-secret",
+  "admin_password": "{{admin-password}}",
+  "admin_secret": "{{admin-secret}}",
   "apps_domain": "bosh-lite.com",
   "skip_ssl_validation": true,
   "test_app_instances": 2,
@@ -23,6 +26,11 @@ echo '
   "extra_listen_ports": 2,
   "prefix":"test-"
 }
-' > $CONFIG
+' > ${CONFIG}
+
+ADMIN_PASSWORD=`grep uaa_scim_users_admin_password ${VARS_STORE} | cut -d' ' -f2`
+sed -i -- "s/{{admin-password}}/${ADMIN_PASSWORD}/g" /tmp/test-config.json
+ADMIN_SECRET=`grep uaa_admin_client_secret ${VARS_STORE} | cut -d' ' -f2`
+sed -i -- "s/{{admin-secret}}/${ADMIN_SECRET}/g" /tmp/test-config.json
 
 ginkgo -v .
