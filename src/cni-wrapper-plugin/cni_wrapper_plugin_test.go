c39783fbde0661d52d6d2c20ac8707d877081c97
Jay Dunkelberger <ldunkelberger@pivotal.io>
2017-03-23 08:17:28 -0700

    Do not add default rules if runtimeConfig is not provided
    [#137552801]
    
    Signed-off-by: Angela Chin <achin@pivotal.io>

--- src/cni-wrapper-plugin/cni_wrapper_plugin_test.go
+++ src/cni-wrapper-plugin/cni_wrapper_plugin_test.go
@@ -128,7 +128,7 @@ var _ = Describe("CniWrapperPlugin", func() {
 				},
 				InstanceAddress:    "10.244.2.3",
 				IPTablesASGLogging: false,
-				RuntimeConfig: lib.RuntimeConfig{
+				RuntimeConfig: &lib.RuntimeConfig{
 					PortMappings: []garden.NetIn{
 						{
 							HostPort:      1000,
@@ -277,6 +277,31 @@ var _ = Describe("CniWrapperPlugin", func() {
 			Eventually(session).Should(gexec.Exit(0))
 			Expect(session.Out.Contents()).To(MatchJSON(`{ "ips": [{ "version": "4", "interface": -1, "address": "1.2.3.4/32" }], "dns":{} }`))
 			Expect(AllIPTablesRules("nat")).To(ContainElement("-A POSTROUTING -s 1.2.3.4/32 ! -d 10.255.0.0/16 -j MASQUERADE"))
+		})
+
+		Context("when no runtime config is passed in", func() {
+			BeforeEach(func() {
+				inputStruct.RuntimeConfig = nil
+				input = GetInput(inputStruct)
+
+				cmd = cniCommand("ADD", input)
+			})
+			It("does not write the default netout rules", func() {
+				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(session).Should(gexec.Exit(0))
+
+				By("checking that there are no netin rules")
+				Expect(AllIPTablesRules("nat")).ToNot(ContainElement(`-N ` + netinChainName))
+				Expect(AllIPTablesRules("nat")).ToNot(ContainElement(`-A PREROUTING -j ` + netinChainName))
+				Expect(AllIPTablesRules("nat")).ToNot(ContainElement("-A " + netinChainName + " -d 10.244.2.3/32 -p tcp -m tcp --dport 1000 -j DNAT --to-destination 1.2.3.4:1001"))
+				Expect(AllIPTablesRules("nat")).ToNot(ContainElement("-A " + netinChainName + " -d 10.244.2.3/32 -p tcp -m tcp --dport 2000 -j DNAT --to-destination 1.2.3.4:2001"))
+
+				By("checking that there are no netout rules")
+				Expect(AllIPTablesRules("filter")).NotTo(ContainElement(ContainSubstring(inputChainName)))
+				Expect(AllIPTablesRules("filter")).NotTo(ContainElement(ContainSubstring(netoutChainName)))
+				Expect(AllIPTablesRules("filter")).NotTo(ContainElement(ContainSubstring(netoutLoggingChainName)))
+			})
 		})
 
 		Describe("PortMapping", func() {
