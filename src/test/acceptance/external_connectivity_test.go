893898f54cf0bb878ec49a358079f72df447e57f
Gabe Rosenhouse <grosenhouse@pivotal.io>
2017-04-04 17:01:44 -0700

    Clean up external connectiviy test
    [#142640345]
    
    Signed-off-by: David McClure <dmcclure@pivotal.io>

--- src/test/acceptance/external_connectivity_test.go
+++ src/test/acceptance/external_connectivity_test.go
@@ -74,165 +74,98 @@ var _ = Describe("external connectivity", func() {
 
 	Describe("basic (legacy) network behavior for an app", func() {
 		It("is reachable from the router, and can reach the internet only if allowed", func(done Done) {
-			By("checking that the app is reachable via the router", func() {
-				Consistently(func() bool {
-					resp, err := http.Get(appRoute)
-					Expect(err).NotTo(HaveOccurred())
-					defer resp.Body.Close()
-					Expect(resp.StatusCode).To(Equal(200))
-					respBytes, err := ioutil.ReadAll(resp.Body)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(respBytes).To(ContainSubstring(`{"ListenAddresses":[`))
-					return true
-				}, "10s", "1s").Should(BeTrue())
-			})
-
-			By("checking that the app can reach the internet", func() {
-				Consistently(func() bool {
-					resp, err := http.Get(appRoute + "proxy/example.com")
-					Expect(err).NotTo(HaveOccurred())
-					defer resp.Body.Close()
-					Expect(resp.StatusCode).To(Equal(200))
-					respBytes, err := ioutil.ReadAll(resp.Body)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(respBytes).To(ContainSubstring("Example Domain"))
-					return true
-				}, "10s", "1s").Should(BeTrue())
-			})
-
-			By("checking that the app can ping the internet", func() {
-				Consistently(func() bool {
-					resp, err := http.Get(appRoute + "ping/example.com")
-					Expect(err).NotTo(HaveOccurred())
-					defer resp.Body.Close()
-					Expect(resp.StatusCode).To(Equal(200))
-					respBytes, err := ioutil.ReadAll(resp.Body)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(respBytes).To(ContainSubstring("Ping succeeded"))
-					return true
-				}, "10s", "1s").Should(BeTrue())
-			})
-
-			By("removing all the original security groups", func() {
-				for _, sg := range originalRunningSecurityGroups {
-					Expect(cf.Cf("unbind-running-security-group", sg).Wait(Timeout_Short)).To(gexec.Exit(0))
-				}
-			})
-
-			By("restarting the app", func() {
-				Expect(cf.Cf("restart", appA).Wait(Timeout_Push)).To(gexec.Exit(0))
-			})
-
-			By("checking that the app cannot reach the internet using http and dns", func() {
-				Consistently(func() bool {
-					resp, err := http.Get(appRoute + "proxy/example.com")
-					Expect(err).NotTo(HaveOccurred())
-					defer resp.Body.Close()
-					Expect(resp.StatusCode).To(Equal(500))
-					respBytes, err := ioutil.ReadAll(resp.Body)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(respBytes).To(ContainSubstring("example.com"))
-					return true
-				}, "5s", "1s").Should(BeTrue())
-			})
-
-			By("checking that the app cannot ping the internet", func() {
-				Consistently(func() bool {
-					resp, err := http.Get(appRoute + "ping/example.com")
-					Expect(err).NotTo(HaveOccurred())
-					defer resp.Body.Close()
-					Expect(resp.StatusCode).To(Equal(500))
-					respBytes, err := ioutil.ReadAll(resp.Body)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(respBytes).To(ContainSubstring("Ping failed to destination: example.com"))
-					return true
-				}, "5s", "1s").Should(BeTrue())
-			})
-
-			By("creating and binding a tcp and udp security group", func() {
-				var err error
-				tcpASGFile, err = testsupport.CreateASGFile(tcpASG())
+			checkRequest := func(route string, expectedStatusCode int, expectedResponseSubstring string) bool {
+				resp, err := http.Get(route)
 				Expect(err).NotTo(HaveOccurred())
-				Expect(cli.CreateSecurityGroup("tcp-asg", tcpASGFile)).To(Succeed())
-				Expect(cli.BindSecurityGroup("tcp-asg", orgName, spaceName)).To(Succeed())
-
-				udpASGFile, err = testsupport.CreateASGFile(udpASG())
+				defer resp.Body.Close()
+				Expect(resp.StatusCode).To(Equal(expectedStatusCode))
+				respBytes, err := ioutil.ReadAll(resp.Body)
 				Expect(err).NotTo(HaveOccurred())
-				Expect(cli.CreateSecurityGroup("udp-asg", udpASGFile)).To(Succeed())
-				Expect(cli.BindSecurityGroup("udp-asg", orgName, spaceName)).To(Succeed())
-			})
+				Expect(respBytes).To(ContainSubstring(expectedResponseSubstring))
+				return true
+			}
 
-			By("restarting the app", func() {
-				Expect(cf.Cf("restart", appA).Wait(Timeout_Push)).To(gexec.Exit(0))
-			})
+			canProxy := func() bool {
+				return checkRequest(appRoute+"proxy/example.com", 200, "Example Domain")
+			}
+			isReachable := func() bool {
+				return checkRequest(appRoute, 200, `{"ListenAddresses":[`)
+			}
+			canPing := func() bool {
+				return checkRequest(appRoute+"ping/example.com", 200, "Ping succeeded")
+			}
+			cannotProxy := func() bool {
+				return checkRequest(appRoute+"proxy/example.com", 500, "example.com")
+			}
+			cannotPing := func() bool {
+				return checkRequest(appRoute+"ping/example.com", 500, "Ping failed to destination: example.com")
+			}
 
-			By("checking that the app can use dns and http to reach the internet", func() {
-				Consistently(func() bool {
-					resp, err := http.Get(appRoute + "proxy/example.com")
-					Expect(err).NotTo(HaveOccurred())
-					defer resp.Body.Close()
-					Expect(resp.StatusCode).To(Equal(200))
-					respBytes, err := ioutil.ReadAll(resp.Body)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(respBytes).To(ContainSubstring("Example Domain"))
-					return true
-				}, "5s", "1s").Should(BeTrue())
-			})
+			By("checking that the app is reachable via the router")
+			Eventually(isReachable, "10s", "1s").Should(BeTrue())
+			Consistently(isReachable, "2s", "0.5s").Should(BeTrue())
 
-			By("checking that the app cannot ping the internet", func() {
-				Consistently(func() bool {
-					resp, err := http.Get(appRoute + "ping/example.com")
-					Expect(err).NotTo(HaveOccurred())
-					defer resp.Body.Close()
-					Expect(resp.StatusCode).To(Equal(500))
-					respBytes, err := ioutil.ReadAll(resp.Body)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(respBytes).To(ContainSubstring("Ping failed to destination: example.com"))
-					return true
-				}, "5s", "1s").Should(BeTrue())
-			})
+			By("checking that the app can reach the internet")
+			Consistently(canProxy, "2s", "0.5s").Should(BeTrue())
 
-			By("creating and binding an icmp security group", func() {
-				var err error
-				icmpASGFile, err = testsupport.CreateASGFile(icmpASG())
-				Expect(err).NotTo(HaveOccurred())
-				Expect(cli.CreateSecurityGroup("icmp-asg", icmpASGFile)).To(Succeed())
-				Expect(cli.BindSecurityGroup("icmp-asg", orgName, spaceName)).To(Succeed())
-			})
+			By("checking that the app can ping the internet")
+			Consistently(canPing, "2s", "0.5s").Should(BeTrue())
 
-			By("removing the tcp security groups", func() {
-				Expect(cli.UnbindSecurityGroup("tcp-asg", orgName, spaceName)).To(Succeed())
-			})
+			By("removing all the original security groups")
+			for _, sg := range originalRunningSecurityGroups {
+				Expect(cf.Cf("unbind-running-security-group", sg).Wait(Timeout_Short)).To(gexec.Exit(0))
+			}
 
-			By("restarting the app", func() {
-				Expect(cf.Cf("restart", appA).Wait(Timeout_Push)).To(gexec.Exit(0))
-			})
+			By("restarting the app")
+			Expect(cf.Cf("restart", appA).Wait(Timeout_Push)).To(gexec.Exit(0))
 
-			By("checking that the app cannot use http to reach the internet", func() {
-				Consistently(func() bool {
-					resp, err := http.Get(appRoute + "proxy/example.com")
-					Expect(err).NotTo(HaveOccurred())
-					defer resp.Body.Close()
-					Expect(resp.StatusCode).To(Equal(500))
-					respBytes, err := ioutil.ReadAll(resp.Body)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(respBytes).To(ContainSubstring("example.com"))
-					return true
-				}, "5s", "1s").Should(BeTrue())
-			})
+			By("checking that the app cannot reach the internet using http and dns")
+			Eventually(cannotProxy, "10s", "1s").Should(BeTrue())
+			Consistently(cannotProxy, "2s", "0.5s").Should(BeTrue())
 
-			By("checking that the app can ping the internet", func() {
-				Consistently(func() bool {
-					resp, err := http.Get(appRoute + "ping/example.com")
-					Expect(err).NotTo(HaveOccurred())
-					defer resp.Body.Close()
-					Expect(resp.StatusCode).To(Equal(200))
-					respBytes, err := ioutil.ReadAll(resp.Body)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(respBytes).To(ContainSubstring("Ping succeeded"))
-					return true
-				}, "5s", "1s").Should(BeTrue())
-			})
+			By("checking that the app cannot ping the internet")
+			Consistently(cannotPing, "2s", "0.5s").Should(BeTrue())
+
+			By("creating and binding a tcp and udp security group")
+			var err error
+			tcpASGFile, err = testsupport.CreateASGFile(tcpASG())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cli.CreateSecurityGroup("tcp-asg", tcpASGFile)).To(Succeed())
+			Expect(cli.BindSecurityGroup("tcp-asg", orgName, spaceName)).To(Succeed())
+
+			udpASGFile, err = testsupport.CreateASGFile(udpASG())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cli.CreateSecurityGroup("udp-asg", udpASGFile)).To(Succeed())
+			Expect(cli.BindSecurityGroup("udp-asg", orgName, spaceName)).To(Succeed())
+
+			By("restarting the app")
+			Expect(cf.Cf("restart", appA).Wait(Timeout_Push)).To(gexec.Exit(0))
+
+			By("checking that the app can use dns and http to reach the internet")
+			Eventually(canProxy, "10s", "1s").Should(BeTrue())
+			Consistently(canProxy, "2s", "0.5s").Should(BeTrue())
+
+			By("checking that the app cannot ping the internet")
+			Consistently(cannotPing, "2s", "1s").Should(BeTrue())
+
+			By("creating and binding an icmp security group")
+			icmpASGFile, err = testsupport.CreateASGFile(icmpASG())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cli.CreateSecurityGroup("icmp-asg", icmpASGFile)).To(Succeed())
+			Expect(cli.BindSecurityGroup("icmp-asg", orgName, spaceName)).To(Succeed())
+
+			By("removing the tcp security groups")
+			Expect(cli.UnbindSecurityGroup("tcp-asg", orgName, spaceName)).To(Succeed())
+
+			By("restarting the app")
+			Expect(cf.Cf("restart", appA).Wait(Timeout_Push)).To(gexec.Exit(0))
+
+			By("checking that the app can ping the internet")
+			Eventually(canPing, "10s", "1s").Should(BeTrue())
+			Consistently(canPing, "2s", "0.5s").Should(BeTrue())
+
+			By("checking that the app cannot use http to reach the internet")
+			Consistently(cannotProxy, "2s", "0.5s").Should(BeTrue())
 
 			close(done)
 		}, 180 /* <-- overall spec timeout in seconds */)
