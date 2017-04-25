4f35b2d7f21fcddec81a79c997270d8b448d1578
Gabe Rosenhouse <grosenhouse@pivotal.io>
2017-04-24 19:36:18 -0700

    clean up test script

--- scripts/unit-and-integration-tests.sh
+++ scripts/unit-and-integration-tests.sh
@@ -21,57 +21,40 @@ declare -a serial_packages=(
   "src/vxlan-policy-agent"
   )
 
-function bootPostgres {
-	echo -n "booting postgres"
-	(/docker-entrypoint.sh postgres &> /var/log/postgres-boot.log) &
-	trycount=0
-	for i in `seq 1 30`; do
-		set +e
-		psql -h localhost -U postgres -c '\conninfo' &>/dev/null
-		exitcode=$?
-		set -e
-		if [ $exitcode -eq 0 ]; then
-			echo "connection established to postgres"
-			return 0
-		fi
-		echo -n "."
-		sleep 1
-	done
-	echo "unable to connect to postgres"
-	exit 1
-}
+function bootDB {
+  db=$1
 
-function bootMysql {
-	echo -n "booting mysql"
-	(MYSQL_ROOT_PASSWORD=password  /entrypoint.sh mysqld &> /var/log/postgres-boot.log) &
-	trycount=0
-	for i in `seq 1 30`; do
-		set +e
-		echo '\s;' | mysql -h 127.0.0.1 -u root --password="password" &>/dev/null
-		exitcode=$?
-		set -e
-		if [ $exitcode -eq 0 ]; then
-			echo "connection established to mysql"
-			return 0
-		fi
-		echo -n "."
-		sleep 1
-	done
-	echo "unable to connect to mysql"
-	exit 1
-}
+  if [ "$db" = "postgres" ]; then
+    launchDB="(/docker-entrypoint.sh postgres &> /var/log/postgres-boot.log) &"
+    testConnection="psql -h localhost -U postgres -c '\conninfo' &>/dev/null"
+  elif [ "$db" = "mysql" ]; then
+    launchDB="(MYSQL_ROOT_PASSWORD=password /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
+    testConnection="echo '\s;' | mysql -h 127.0.0.1 -u root --password='password' &>/dev/null"
+  else
+    echo "skipping database"
+    return 0
+  fi
 
-db=${DB:-"notset"}
-if [ $db = "none" ]; then
-  echo "skipping database"
-elif [ $db = "mysql" ]; then
-  bootMysql
-elif [ $db = "postgres" ]; then
-  bootPostgres
-elif [ $db = "notset" ]; then
-  echo "missing required env var DB"
+  echo -n "booting $db"
+  eval "$launchDB"
+  trycount=0
+  for i in `seq 1 30`; do
+    set +e
+    eval "$testConnection"
+    exitcode=$?
+    set -e
+    if [ $exitcode -eq 0 ]; then
+      echo "connection established to $db"
+      return 0
+    fi
+    echo -n "."
+    sleep 1
+  done
+  echo "unable to connect to $db"
   exit 1
-fi
+}
+
+bootDB "${DB:-"notset"}"
 
 if [ "${1:-""}" = "" ]; then
   for dir in "${packages[@]}"; do
@@ -85,8 +68,5 @@ if [ "${1:-""}" = "" ]; then
     popd
   done
 else
-  testdir="$1"
-  pushd $testdir
-    ginkgo -r -randomizeAllSpecs -randomizeSuites "${@:2}"
-  popd
+  ginkgo -r -randomizeAllSpecs -randomizeSuites "${@}"
 fi
