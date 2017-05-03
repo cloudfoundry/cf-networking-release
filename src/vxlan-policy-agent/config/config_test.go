e0cebe223f28d03c7d3d684d3ff63f8f15e412dd
David McClure <dmcclure@pivotal.io>
2017-05-03 16:16:59 -0700

    Fix test for VXLAN config validation
    [#144492927]

--- src/vxlan-policy-agent/config/config_test.go
+++ src/vxlan-policy-agent/config/config_test.go
@@ -39,7 +39,8 @@ var _ = Describe("Config", func() {
 					"debug_server_host": "http://5.6.7.8",
 					"debug_server_port": 5678,
 					"log_level": "debug",
-					"iptables_c2c_logging": true
+					"iptables_c2c_logging": true,
+					"client_timeout_seconds":5
 				}`)
 				c, err := config.New(file.Name())
 				Expect(err).NotTo(HaveOccurred())
@@ -56,6 +57,7 @@ var _ = Describe("Config", func() {
 				Expect(c.DebugServerPort).To(Equal(5678))
 				Expect(c.LogLevel).To(Equal("debug"))
 				Expect(c.IPTablesLogging).To(Equal(true))
+				Expect(c.ClientTimeoutSeconds).To(Equal(5))
 			})
 		})
 
@@ -84,17 +86,18 @@ var _ = Describe("Config", func() {
 		DescribeTable("when config file is missing a member",
 			func(missingFlag, errorMsg string) {
 				allData := map[string]interface{}{
-					"poll_interval":      1234,
-					"cni_datastore_path": "/some/datastore/path",
-					"policy_server_url":  "https://some-url:1234",
-					"vni":                42,
-					"metron_address":     "http://1.2.3.4:1234",
-					"ca_cert_file":       "/some/ca/file",
-					"client_cert_file":   "/some/client/cert/file",
-					"client_key_file":    "/some/client/key/file",
-					"iptables_lock_file": "/var/vcap/data/lock",
-					"debug_server_host":  "http://5.6.7.8",
-					"debug_server_port":  5678,
+					"poll_interval":          1234,
+					"cni_datastore_path":     "/some/datastore/path",
+					"policy_server_url":      "https://some-url:1234",
+					"vni":                    42,
+					"metron_address":         "http://1.2.3.4:1234",
+					"ca_cert_file":           "/some/ca/file",
+					"client_cert_file":       "/some/client/cert/file",
+					"client_key_file":        "/some/client/key/file",
+					"iptables_lock_file":     "/var/vcap/data/lock",
+					"debug_server_host":      "http://5.6.7.8",
+					"debug_server_port":      5678,
+					"client_timeout_seconds": 5,
 				}
 				delete(allData, missingFlag)
 				Expect(json.NewEncoder(file).Encode(allData)).To(Succeed())
@@ -113,6 +116,7 @@ var _ = Describe("Config", func() {
 			Entry("missing iptables lock file", "iptables_lock_file", "IPTablesLockFile: zero value"),
 			Entry("missing debug server host", "debug_server_host", "DebugServerHost: zero value"),
 			Entry("missing debug server port", "debug_server_port", "DebugServerPort: zero value"),
+			Entry("missing client timeout", "client_timeout_seconds", "ClientTimeoutSeconds: zero value"),
 		)
 	})
 })
