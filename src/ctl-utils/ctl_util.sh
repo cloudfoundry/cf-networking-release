38b0042e48cb9226d23702a3a3440bb0588a841f
Angela Chin <achin@pivotal.io>
2017-05-09 11:03:57 -0700

    Clean up bash
    - Use shell check to improve bash scripts
    
    [#144596281]
    
    Signed-off-by: David McClure <dmcclure@pivotal.io>

--- src/ctl-utils/ctl_util.sh
+++ src/ctl-utils/ctl_util.sh
@@ -3,9 +3,9 @@
 function wait_for_server_to_become_healthy() {
   local url=$1
   local timeout=$2
-  for i in $(seq $timeout); do
+  for _ in $(seq "${timeout}"); do
     set +e
-    curl -f --connect-timeout 1 $url > /dev/null 2>&1
+    curl -f --connect-timeout 1 "${url}" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
       echo 0
       return
@@ -28,10 +28,10 @@ function wait_pid() {
   local pid=$1
   local max_checks=$2
 
-  checks=$max_checks
-  while [ -e "/proc/$pid" ]; do
+  checks="${max_checks}"
+  while [ -e /proc/"${pid}" ]; do
     checks=$((checks - 1))
-    if [ $checks -le 0 ]; then
+    if [ "${checks}" -le 0 ]; then
       return 1
     fi
     sleep 0.1
@@ -42,26 +42,26 @@ function wait_pid() {
 
 function stop_process() {
   local pid
-  pid=$(cat "$PIDFILE")
+  pid=$(cat "${PIDFILE}")
 
   echo "stopping..."
-  kill -TERM "$pid"
-  if wait_pid "$pid" 100 ; then
-    rm "$PIDFILE"
+  kill -TERM "${pid}"
+  if wait_pid "${pid}" 100 ; then
+    rm "${PIDFILE}"
     return 0
   fi
 
   echo "unable to stop process using SIGTERM after 10 seconds, will now attempt to SIGQUIT"
-  kill -QUIT "$pid" || true
-  if wait_pid "$pid" 50 ; then
-    rm "$PIDFILE"
+  kill -QUIT "${pid}" || true
+  if wait_pid "${pid}" 50 ; then
+    rm "${PIDFILE}"
     return 0
   fi
 
   echo "unable to stop process using SIGQUIT after 5 seconds, will now attempt to SIGKILL"
-  kill -KILL "$pid" || true
-  if wait_pid "$pid" 50 ; then
-    rm "$PIDFILE"
+  kill -KILL "${pid}" || true
+  if wait_pid "${pid}" 50 ; then
+    rm "${PIDFILE}"
     return 0
   fi
 
@@ -73,10 +73,10 @@ function write_pid() {
   local healthy=$1
   local pid=$2
 
-  if [ ${healthy} -eq 0 ]; then
-    echo ${pid} > ${PIDFILE}
+  if [ "${healthy}" -eq 0 ]; then
+    echo "${pid}" > "${PIDFILE}"
   else
-    kill -9 ${pid}
+    kill -9 "${pid}"
     exit 1
   fi
 }
