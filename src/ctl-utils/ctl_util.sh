94039f4f8015fe73a044094bf8b754e4bd4ee6ee
David McClure <dmcclure@pivotal.io>
2017-05-09 14:25:54 -0700

    Upon start, kill any process listening on health check port
    [#145063947]
    
    Signed-off-by: Angela Chin <achin@pivotal.io>

--- src/ctl-utils/ctl_util.sh
+++ src/ctl-utils/ctl_util.sh
@@ -20,8 +20,75 @@ function wait_for_server_to_become_healthy() {
 function handle_orphaned_server() {
   local job_name=$1
   local pid=$2
+
   echo "killing ${job_name} with pid ${pid}"
   kill "${pid}"
+}
+
+function stop_process() {
+  local pid
+  pid=$(cat "${PIDFILE}")
+  stop_gracefully "${pid}"
+}
+
+stop_process_on_port() {
+  local port=$1
+
+  echo "checking for processes listening on port ${port}"
+  set +e
+  local pids
+  pids=$(lsof -t -i :"${port}")
+  set -e
+  if [ ! -z "${pids}" ]; then
+    echo "the following processes are listening on port ${port}"
+    lsof -i :"${port}"
+  else
+    echo "no processes found listening on port ${port}"
+  fi
+
+  for pid in ${pids}; do
+    echo "killing process with pid ${pid}"
+    stop_gracefully "${pid}"
+  done
+}
+
+stop_gracefully() {
+  local pid=$1
+  echo "stopping..."
+  kill -TERM "${pid}"
+  if wait_pid "${pid}" 80 ; then
+    rm "${PIDFILE}"
+    return 0
+  fi
+
+  echo "unable to stop process using SIGTERM after 8 seconds, will now attempt to SIGQUIT"
+  kill -QUIT "${pid}" || true
+  if wait_pid "${pid}" 20 ; then
+    rm "${PIDFILE}"
+    return 0
+  fi
+
+  echo "unable to stop process using SIGQUIT after 2 seconds, will now attempt to SIGKILL"
+  kill -KILL "${pid}" || true
+  if wait_pid "${pid}" 20 ; then
+    rm "${PIDFILE}"
+    return 0
+  fi
+
+  echo "unable to stop process using SIGKILL after 2 seconds"
+  return 1
+}
+
+function write_pid() {
+  local healthy=$1
+  local pid=$2
+
+  if [ "${healthy}" -eq 0 ]; then
+    echo "${pid}" > "${PIDFILE}"
+  else
+    kill -9 "${pid}"
+    exit 1
+  fi
 }
 
 function wait_pid() {
@@ -38,45 +105,4 @@ function wait_pid() {
   done
 
   return 0
-}
-
-function stop_process() {
-  local pid
-  pid=$(cat "${PIDFILE}")
-
-  echo "stopping..."
-  kill -TERM "${pid}"
-  if wait_pid "${pid}" 100 ; then
-    rm "${PIDFILE}"
-    return 0
-  fi
-
-  echo "unable to stop process using SIGTERM after 10 seconds, will now attempt to SIGQUIT"
-  kill -QUIT "${pid}" || true
-  if wait_pid "${pid}" 50 ; then
-    rm "${PIDFILE}"
-    return 0
-  fi
-
-  echo "unable to stop process using SIGQUIT after 5 seconds, will now attempt to SIGKILL"
-  kill -KILL "${pid}" || true
-  if wait_pid "${pid}" 50 ; then
-    rm "${PIDFILE}"
-    return 0
-  fi
-
-  echo "unable to stop process using SIGKILL after 5 seconds"
-  return 1
-}
-
-function write_pid() {
-  local healthy=$1
-  local pid=$2
-
-  if [ "${healthy}" -eq 0 ]; then
-    echo "${pid}" > "${PIDFILE}"
-  else
-    kill -9 "${pid}"
-    exit 1
-  fi
 }
