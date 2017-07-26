c3d80a18c0f4261ad9e0b8fc3df418467033d0a2
Dennis Leon <dleon@pivotal.io>
2017-07-26 15:00:17 -0700

    Add logic to consider the rep/silk-daemon shutdown unable to connect
    - refactor impl and test
    - close response body
    - consider a timeout / temporary error as the server is still up
    
    [#148173051]
    Signed-off-by: David McClure <dmcclure@pivotal.io>

--- src/silk-daemon-teardown/integration/integration_test.go
+++ src/silk-daemon-teardown/integration/integration_test.go
@@ -6,120 +6,214 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
-	"net/http"
-	"net/http/httptest"
 	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/ghttp"
+	"os"
+	"io/ioutil"
+	"strconv"
+	"net/http"
+	"time"
 )
 
 var (
-	DEFAULT_TIMEOUT = "5s"
-
+	DEFAULT_TIMEOUT = "10s"
 )
 
-var fakeRepServer *httptest.Server
-var fakeSilkDaemonServer *httptest.Server
-var numberFakeServerHits int
-var numberSilkDaemonHits int
+var fakeRepServer *ghttp.Server
+var fakeSilkDaemonServer *ghttp.Server
+var tempPidFile *os.File
+var fakeSilkDaemonSession *gexec.Session
 
 var _ = BeforeEach(func() {
-	fakeRepServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if numberFakeServerHits == 0 {
-			w.WriteHeader(200)
-		} else {
-			w.WriteHeader(400)
-		}
-		numberFakeServerHits++
-	}))
+	fakeRepServer = ghttp.NewServer()
+	fakeSilkDaemonServer = ghttp.NewServer()
+	fakeRepServer.AllowUnhandledRequests = true
+	fakeSilkDaemonServer.AllowUnhandledRequests = true
+	fakeRepServer.UnhandledRequestStatusCode = 500
+	fakeSilkDaemonServer.UnhandledRequestStatusCode = 500
 
-	fakeSilkDaemonServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if numberSilkDaemonHits == 0 {
-			w.WriteHeader(200)
-		} else {
-			w.WriteHeader(400)
-		}
-		numberSilkDaemonHits++
-	}))
+	fakeRepServer.AppendHandlers(ghttp.RespondWith(200, "", nil))
+	fakeSilkDaemonServer.AppendHandlers(ghttp.RespondWith(200, "", nil))
 })
 
 var _ = AfterEach(func() {
-	numberFakeServerHits = 0
-	numberSilkDaemonHits = 0
+	fakeRepServer.Close()
+	fakeSilkDaemonServer.Close()
+
+	if tempPidFile != nil {
+		os.RemoveAll(tempPidFile.Name())
+	}
 })
 
 var _ = Describe("Teardown", func() {
+	BeforeEach(func() {
+		var err error
+		tempPidFile, err = ioutil.TempFile(os.TempDir(), "pid")
+		Expect(err).NotTo(HaveOccurred())
+		sleepCommand := exec.Command("sleep", "60")
 
-	It("pings the silk daemon", func() {
-		session := runTeardown(fakeRepServer.URL, fakeSilkDaemonServer.URL)
-		Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(0))
+		fakeSilkDaemonSession, err = gexec.Start(sleepCommand, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
 
-		Expect(numberSilkDaemonHits).To(Equal(2))
+		Expect(ioutil.WriteFile(tempPidFile.Name(), []byte(strconv.Itoa(sleepCommand.Process.Pid)), 0777)).To(Succeed())
 	})
 
-	It("pings the rep", func() {
-		session := runTeardown(fakeRepServer.URL, fakeSilkDaemonServer.URL)
-		Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(0))
+	Context("when the servers eventually shutdown", func() {
+		BeforeEach(func() {
+			fakeRepServer.AppendHandlers(http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+				go func() {
+					println("shutting down rep")
+					fakeRepServer.Close()
+				}()
+			}))
 
-		Expect(numberFakeServerHits).To(Equal(2))
-	})
+			fakeSilkDaemonServer.AppendHandlers(http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+				go func() {
+					println("shutting down silk")
+					fakeSilkDaemonServer.Close()
+				}()
+			}))
+		})
 
-	It("pings the rep until the rep exits", func() {
-		session := runTeardown(fakeRepServer.URL, fakeSilkDaemonServer.URL)
-		Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(0))
+		It("kills the silk-daemon and pings the silk daemon until it stops responding", func() {
+			session := runTeardown(fakeRepServer.URL(), fakeSilkDaemonServer.URL(), tempPidFile.Name())
+			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(0))
 
-		Expect(numberFakeServerHits).To(Equal(2))
-		Expect(session.Out).To(gbytes.Say("waiting for the rep to exit"))
-
-	})
-
-	It("pings the silk daemon until it exits", func() {
-		session := runTeardown(fakeRepServer.URL, fakeSilkDaemonServer.URL)
-		Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(0))
-
-		Expect(numberSilkDaemonHits).To(Equal(2))
-		Expect(session.Out).To(gbytes.Say("waiting for the silk daemon to exit"))
-	})
-
-	Context("when connecting to the rep fails", func() {
-		It("returns an error", func() {
-			session := runTeardown("some/bad/url", fakeSilkDaemonServer.URL)
-			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(1))
-
-			Expect(session.Err).To(gbytes.Say("silk-daemon-teardown: pinging rep failed with: Get some/bad/url: unsupported protocol scheme"))
+			Expect(fakeRepServer.ReceivedRequests()).To(HaveLen(2))
+			Expect(session.Out).To(gbytes.Say("waiting for the rep to exit"))
+			Expect(fakeRepServer.ReceivedRequests()).To(HaveLen(2))
+			Expect(session.Out).To(gbytes.Say("waiting for the silk daemon to exit"))
+			Eventually(fakeSilkDaemonSession.ExitCode(), "5s").Should(Equal(143))
 		})
 	})
 
-	Context("when connecting to the silk daemon fails", func() {
+	Context("pinged servers return non 200 status codes", func() {
+		It("pings the rep until the rep returns non 200 status code", func() {
+			session := runTeardown(fakeRepServer.URL(), fakeSilkDaemonServer.URL(), tempPidFile.Name())
+			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(0))
+
+			Expect(fakeRepServer.ReceivedRequests()).To(HaveLen(2))
+			Expect(session.Out).To(gbytes.Say("waiting for the rep to exit"))
+			Eventually(fakeSilkDaemonSession.ExitCode(), "5s").Should(Equal(143))
+		})
+
+		It("pings the silk daemon until it returns non 200 status code", func() {
+			session := runTeardown(fakeRepServer.URL(), fakeSilkDaemonServer.URL(), tempPidFile.Name())
+			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(0))
+
+			Expect(fakeRepServer.ReceivedRequests()).To(HaveLen(2))
+			Expect(session.Out).To(gbytes.Say("waiting for the silk daemon to exit"))
+			Eventually(fakeSilkDaemonSession.ExitCode(), "5s").Should(Equal(143))
+		})
+	})
+
+	Context("when connecting to the rep fails due to a bad url", func() {
 		It("returns an error", func() {
-			session := runTeardown(fakeRepServer.URL, "some/bad/url")
+			session := runTeardown("some/bad/url", fakeSilkDaemonServer.URL(), tempPidFile.Name())
 			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(1))
 
-			Expect(session.Err).To(gbytes.Say("silk-daemon-teardown: pinging silk-daemon failed with: Get some/bad/url: unsupported protocol scheme"))
+			Expect(session.Err).To(gbytes.Say("silk-daemon-teardown: parse some/bad/url: invalid URI for request"))
+		})
+	})
 
+	Context("when connecting to the silk-daemon fails due to a bad url", func() {
+		It("returns an error", func() {
+			session := runTeardown(fakeRepServer.URL(), "some/bad/url", tempPidFile.Name())
+			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(1))
+
+			Expect(session.Err).To(gbytes.Say("silk-daemon-teardown: parse some/bad/url: invalid URI for request"))
+		})
+	})
+
+	Context("when pinging the rep takes a long time to reply", func() {
+		BeforeEach(func() {
+			fakeRepServer.AppendHandlers(http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+				time.Sleep(10 * time.Second)
+			}))
+		})
+
+		It("should timeout pinging rep and take less than 10 seconds to finish", func() {
+			session := runTeardown(fakeRepServer.URL(), fakeSilkDaemonServer.URL(), tempPidFile.Name())
+			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(0))
+
+			Expect(len(fakeRepServer.ReceivedRequests())).To(Equal(3))
+			Expect(session.Out).To(gbytes.Say("pinging server timed out. trying again."))
+			Expect(session.Out).To(gbytes.Say("waiting for the rep to exit"))
 		})
 	})
 
 	Context("When silk daemon will not exit", func() {
 		BeforeEach(func() {
-			fakeSilkDaemonServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-				w.WriteHeader(200)
-				numberSilkDaemonHits++
-			}))
+			fakeSilkDaemonServer.UnhandledRequestStatusCode = 200
 		})
 
 		It("pings the silk daemon server 5 times and fails gracefully", func() {
-			session := runTeardown(fakeRepServer.URL, fakeSilkDaemonServer.URL)
+			session := runTeardown(fakeRepServer.URL(), fakeSilkDaemonServer.URL(), tempPidFile.Name())
 			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(1))
 
-			Expect(numberSilkDaemonHits).To(Equal(5))
+			Expect(fakeSilkDaemonServer.ReceivedRequests()).To(HaveLen(5))
 			Expect(session.Err).To(gbytes.Say("silk-daemon-teardown: Silk Daemon Server did not exit after 5 ping attempts"))
+		})
+	})
+
+	Context("When rep server will not exit", func() {
+		BeforeEach(func() {
+			fakeRepServer.UnhandledRequestStatusCode = 200
+		})
+
+		It("pings the rep server 40 times and fails gracefully", func() {
+			session := runTeardown(fakeRepServer.URL(), fakeSilkDaemonServer.URL(), tempPidFile.Name())
+			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(0))
+
+			Expect(fakeRepServer.ReceivedRequests()).To(HaveLen(40))
+			Expect(session.Out).To(gbytes.Say("rep did not exit after 40 ping attempts"))
+
+		}, 5)
+	})
+
+	Context("when silk daemon pid file does not exist", func() {
+		It("returns an error", func() {
+			session := runTeardown(fakeRepServer.URL(), fakeSilkDaemonServer.URL(), "/some-invalid/file-path")
+			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(1))
+
+			Expect(session.Err).To(gbytes.Say("silk-daemon-teardown: open /some-invalid/file-path: no such file or directory"))
+		})
+	})
+
+	Context("when the silk daemon is not running", func() {
+		BeforeEach(func() {
+			fakeSilkDaemonSession.Kill()
+			Eventually(fakeSilkDaemonSession).Should(gexec.Exit())
+		})
+
+		It("returns with exit code 0", func() {
+			session := runTeardown(fakeRepServer.URL(), fakeSilkDaemonServer.URL(), tempPidFile.Name())
+			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(0))
+		})
+	})
+
+	Context("when silk daemon pid file does not contain a number", func() {
+		BeforeEach(func() {
+			Expect(ioutil.WriteFile(tempPidFile.Name(), []byte("not-a-number"), 0777)).To(Succeed())
+		})
+
+		It("returns an error", func() {
+			session := runTeardown(fakeRepServer.URL(), fakeSilkDaemonServer.URL(), tempPidFile.Name())
+			Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit(1))
+
+			Expect(session.Err).To(gbytes.Say("silk-daemon-teardown: strconv.Atoi: parsing \"not-a-number\": invalid syntax"))
 		})
 	})
 })
 
-func runTeardown(url, silkDaemonUrl string) *gexec.Session {
-	startCmd := exec.Command(paths.TeardownBin, "--repUrl", url, "--silkDaemonUrl", silkDaemonUrl, "--repTimeout", "0", "--silkDaemonTimeout", "0")
+func runTeardown(url, silkDaemonUrl string, silkDaemonPidFile string) *gexec.Session {
+	startCmd := exec.Command(paths.TeardownBin,
+		"--repUrl", url,
+		"--silkDaemonUrl", silkDaemonUrl,
+		"--repTimeout", "0",
+		"--silkDaemonTimeout", "0",
+		"--silkDaemonPidPath", silkDaemonPidFile)
 	session, err := gexec.Start(startCmd, GinkgoWriter, GinkgoWriter)
 	Expect(err).NotTo(HaveOccurred())
-	Eventually(session, DEFAULT_TIMEOUT).Should(gexec.Exit())
 	return session
 }
--- src/silk-daemon-teardown/main.go
+++ src/silk-daemon-teardown/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
-	"flag"
-	"fmt"
+	"syscall"
+	"io/ioutil"
+	"strconv"
+	"net"
+	neturl "net/url"
 	"errors"
 )
 
@@ -15,58 +20,107 @@ func main() {
 	}
 }
 
-func pingServer(url, hostType string) (int, error) {
-	httpClient := &http.Client{
-		Transport: &http.Transport{},
-		Timeout: 5 * time.Second,
-	}
-
-	response, err := httpClient.Get(url)
-	if err != nil {
-		return 0, fmt.Errorf("pinging %s failed with: %s", hostType, err)
-	}
-
-	return response.StatusCode, err
-}
-
 func mainWithError() error {
 	repUrl := flag.String("repUrl", "", "path to rep url")
-	repTimeout := flag.Int("repTimeout", 5, "timeout inbetween calls to rep")
-
+	repTimeout := flag.Int("repTimeout", 5, "timeout (seconds) between calls to rep")
 	silkDaemonUrl := flag.String("silkDaemonUrl", "", "path to silk daemon url")
-	silkDaemonTimeout := flag.Int("silkDaemonTimeout", 2, "timeout inbetween calls to silk daemon")
+	silkDaemonTimeout := flag.Int("silkDaemonTimeout", 2, "timeout (seconds) between calls to silk daemon")
+	silkDaemonPidPath := flag.String("silkDaemonPidPath", "", "pid file of silk daemon")
+	pingServerTimeout := flag.Int("pingServerTimeout", 300, "timeout (seconds) when pinging if server is up")
 
 	flag.Parse()
 
 	var err error
-	response := 200
-	for response == 200 {
-		response, err = pingServer(*repUrl, "rep")
-		fmt.Println( fmt.Sprintf("%s: waiting for the rep to exit", time.Now()))
-		if err != nil {
-			return err
-		}
-		time.Sleep(time.Duration(*repTimeout) * time.Second)
+	repMaxAttempts := 40
+	isRepUp, err := waitForServer("rep", *repUrl, *repTimeout, repMaxAttempts, *pingServerTimeout)
+	if err != nil {
+		return err
 	}
 
-	response = 200
-	numberOfTimesSilkDaemonServerHit := 0
-	for response == 200 && numberOfTimesSilkDaemonServerHit < 5 {
-		response, err = pingServer(*silkDaemonUrl, "silk-daemon")
-		fmt.Println( fmt.Sprintf("%s: waiting for the silk daemon to exit", time.Now()))
-		if err != nil {
-			return err
-		}
-		time.Sleep(time.Duration(*silkDaemonTimeout) * time.Second)
-		numberOfTimesSilkDaemonServerHit++
+	if isRepUp {
+		fmt.Println(fmt.Sprintf("Rep Server did not exit after %d ping attempts. Continuing", repMaxAttempts))
 	}
 
-	if didSilkDaemonServerExit(numberOfTimesSilkDaemonServerHit) {
-		return errors.New("Silk Daemon Server did not exit after 5 ping attempts")
+	pidFileConents, err := ioutil.ReadFile(*silkDaemonPidPath)
+	if err != nil {
+		return err
+	}
+
+	pid, err := strconv.Atoi(string(pidFileConents))
+	if err != nil {
+		return err
+	}
+
+	_ = syscall.Kill(pid, syscall.SIGTERM)
+
+	silkDaemonMaxAttempts := 5
+	silkDaemonIsUp, err := waitForServer("silk daemon", *silkDaemonUrl, *silkDaemonTimeout, silkDaemonMaxAttempts, *pingServerTimeout)
+	if err != nil {
+		return err
+	}
+	if silkDaemonIsUp {
+		return errors.New(fmt.Sprintf("Silk Daemon Server did not exit after %d ping attempts", silkDaemonMaxAttempts))
 	}
 
 	return nil
 }
-func didSilkDaemonServerExit(numberOfTimesSilkDaemonServerHit int) bool {
-	return numberOfTimesSilkDaemonServerHit == 5
+
+func waitForServer(serverName string, serverUrl string, pollingTimeInSeconds int, maxAttempts int, pingTimeout int) (isServerUp bool, err error) {
+	_, err = neturl.ParseRequestURI(serverUrl)
+	if err != nil {
+		return true, err
+	}
+	currentAttempt := 0
+
+	for currentAttempt < maxAttempts {
+		fmt.Println(fmt.Sprintf("%s: waiting for the %s to exit", time.Now(), serverName))
+
+		select {
+		case <-time.After(time.Duration(pollingTimeInSeconds) * time.Second):
+			if !checkIfServerUp(serverName, serverUrl) {
+				return false, nil
+			}
+			currentAttempt++
+		case <-time.After(time.Duration(pingTimeout) * time.Second):
+			return true, nil
+		}
+	}
+
+	if currentAttempt >= maxAttempts {
+		fmt.Println(fmt.Sprintf("%s: %s did not exit after %d ping attempts. closing pinger", time.Now(), serverName, maxAttempts))
+	}
+
+	return true, nil
+}
+
+func checkIfServerUp(serverName string, url string) bool {
+	httpClient := &http.Client{
+		Transport: &http.Transport{},
+		Timeout:   5 * time.Second,
+	}
+
+	fmt.Println(fmt.Sprintf("pinging %s", url))
+	response, err := httpClient.Get(url)
+
+	if err != nil {
+		if netErr, ok := err.(net.Error); ok {
+			if netErr.Timeout() {
+				fmt.Println(fmt.Sprintf("%s: pinging server timed out. trying again.", time.Now()))
+				return true
+
+			}
+			if netErr.Temporary() {
+				fmt.Println(fmt.Sprintf("%s: pinging server returned temporary error. trying again.", time.Now()))
+				return true
+			}
+		}
+	} else {
+		defer response.Body.Close()
+		if response.StatusCode >= http.StatusOK && response.StatusCode <= http.StatusPartialContent {
+			return true
+		}
+	}
+
+	fmt.Println(fmt.Sprintf("%s: could not ping %s server. Server is down", time.Now(), serverName))
+	return false
 }
