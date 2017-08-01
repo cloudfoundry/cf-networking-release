c852cc77dcbf75776208574b81e8191967d054ef
Dennis Leon <dleon@pivotal.io>
2017-08-01 15:21:46 -0700

    Start mysql server for tests when using mysql 5.6
    [#148173239]
    
    Signed-off-by: David McClure <dmcclure@pivotal.io>

--- scripts/unit-and-integration-tests.sh
+++ scripts/unit-and-integration-tests.sh
@@ -29,7 +29,7 @@ function bootDB {
   if [ "$db" = "postgres" ]; then
     launchDB="(/docker-entrypoint.sh postgres &> /var/log/postgres-boot.log) &"
     testConnection="psql -h localhost -U postgres -c '\conninfo' &>/dev/null"
-  elif [ "$db" = "mysql" ]; then
+  elif [ "$db" = "mysql" ]  || [ "$db" = "mysql-5.6" ]; then
     launchDB="(MYSQL_ROOT_PASSWORD=password /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
     testConnection="echo '\s;' | mysql -h 127.0.0.1 -u root --password='password' &>/dev/null"
   else
