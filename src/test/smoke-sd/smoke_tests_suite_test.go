822097e805681413efdc95698c523ac1539b8713
Christian Ang <cang@pivotal.io>
2018-05-07 15:44:28 -0700

    fix auth for smoke-sd
    [#157125779]
    
    Signed-off-by: Amelia Downs <adowns@pivotal.io>

--- src/test/smoke-sd/smoke_tests_suite_test.go
+++ src/test/smoke-sd/smoke_tests_suite_test.go
@@ -36,10 +36,10 @@ var (
 type SmokeConfig struct {
 	ApiEndpoint       string `json:"api"`
 	AppsDomain        string `json:"apps_domain"`
-	AdminUser         string `json:"admin_user"`
-	AdminPassword     string `json:"admin_password"`
 	Prefix            string `json:"prefix"`
 	SkipSSLValidation bool   `json:"skip_ssl_validation"`
+	SmokeUser         string `json:"smoke_user"`
+	SmokePassword     string `json:"smoke_password"`
 	SmokeOrg          string `json:"smoke_org"`
 	SmokeSpace        string `json:"smoke_space"`
 }
@@ -53,27 +53,20 @@ var _ = BeforeSuite(func() {
 	err = json.Unmarshal(configBytes, &config)
 	Expect(err).NotTo(HaveOccurred())
 
-	// Log in to cf api
-
 	if config.SkipSSLValidation {
 		Expect(cf.Cf("api", "--skip-ssl-validation", config.ApiEndpoint).Wait(Timeout_Short)).To(gexec.Exit(0))
 	} else {
-
 		Expect(cf.Cf("api", config.ApiEndpoint).Wait(Timeout_Short)).To(gexec.Exit(0))
 	}
-	Auth(config.AdminUser, config.AdminPassword)
+
+	Auth(config.SmokeUser, config.SmokePassword)
 
 	// Set env vars
 	appsDir = os.Getenv("APPS_DIR")
 	Expect(appsDir).NotTo(BeEmpty())
 
 	rand.Seed(ginkgoConfig.GinkgoConfig.RandomSeed + int64(GinkgoParallelNode()))
-
 })
-
-func AuthAsAdmin() {
-	Auth(config.AdminUser, config.AdminPassword)
-}
 
 func Auth(username, password string) {
 	By("authenticating as " + username)
