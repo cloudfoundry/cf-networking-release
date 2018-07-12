2bac641e838868009e462080d95d498a77ee4d70
Tyler Schultz <tschultz@pivotal.io>
2018-07-12 10:04:11 -0700

    Stop migrating policy db when constructing stores
    previous commit(s) move migrations to pre-start
    
    [#158395941]
    
    Signed-off-by: Amelia Downs <adowns@pivotal.io>

---
+++ src/policy-server/store/tag_populator.go
@@ -0,0 +1,46 @@
+package store
+
+import (
+	"bytes"
+	"math"
+	"fmt"
+)
+
+type TagPopulator struct {
+	DBConnection database
+}
+
+func (t *TagPopulator) PopulateTables(tl int) error {
+	var err error
+	row := t.DBConnection.QueryRow(`SELECT COUNT(*) FROM groups`)
+	if row != nil {
+		var count int
+		err = row.Scan(&count)
+		if err != nil {
+			return err
+		}
+		if count > 0 {
+			return nil
+		}
+	}
+
+	var b bytes.Buffer
+	_, err = b.WriteString("INSERT INTO groups (guid) VALUES (NULL)")
+	if err != nil {
+		return err
+	}
+
+	for i := 1; i < int(math.Exp2(float64(tl*8)))-1; i++ {
+		_, err = b.WriteString(", (NULL)")
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = t.DBConnection.Exec(b.String())
+	if err != nil {
+		return fmt.Errorf("populating tables: %s", err)
+	}
+
+	return nil
+}
---
+++ src/policy-server/store/tag_populator_test.go
@@ -0,0 +1,118 @@
+package store_test
+
+import (
+	dbHelper "code.cloudfoundry.org/cf-networking-helpers/db"
+	"code.cloudfoundry.org/cf-networking-helpers/testsupport"
+	"code.cloudfoundry.org/lager"
+	"database/sql"
+	"fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"policy-server/db"
+	"policy-server/store"
+	"policy-server/store/fakes"
+	"policy-server/store/migrations"
+	"strings"
+	"test-helpers"
+	"time"
+	"errors"
+)
+
+var _ = Describe("Tag Populator", func() {
+	var (
+		tagPopulator *store.TagPopulator
+	)
+
+	Context("when connecting to the DB succeeds", func() {
+		var (
+			dbConf dbHelper.Config
+			realDb *db.ConnWrapper
+
+			realMigrator *migrations.Migrator
+		)
+
+		BeforeEach(func() {
+
+			dbConf = testsupport.GetDBConfig()
+			dbConf.DatabaseName = fmt.Sprintf("tag_populator_test_node_%d", time.Now().UnixNano())
+
+			testhelpers.CreateDatabase(dbConf)
+
+			logger := lager.NewLogger("Tag Populator Test")
+
+			var err error
+			realDb = db.NewConnectionPool(dbConf, 200, 200, "Tag Populator Test", "Tag Populator Test", logger)
+			Expect(err).NotTo(HaveOccurred())
+
+			realMigrator = &migrations.Migrator{
+				MigrateAdapter: &migrations.MigrateAdapter{},
+			}
+
+			realMigrator.PerformMigrations(realDb.DriverName(), realDb, 0)
+			tagPopulator = &store.TagPopulator{
+				DBConnection: realDb,
+			}
+		})
+
+		AfterEach(func() {
+			if realDb != nil {
+				Expect(realDb.Close()).To(Succeed())
+			}
+			testhelpers.RemoveDatabase(dbConf)
+		})
+
+		Context("when the groups table is being populated", func() {
+			It("does not exceed 2^(tag_length * 8) rows", func() {
+				tagPopulator.PopulateTables(1)
+				var id int
+				err := realDb.QueryRow(`SELECT id FROM groups ORDER BY id DESC LIMIT 1`).Scan(&id)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(id).To(Equal(255))
+			})
+		})
+
+		Context("when the groups table is ALREADY populated", func() {
+			It("does not add more rows", func() {
+				tagPopulator.PopulateTables(1)
+				var id int
+				err := realDb.QueryRow(`SELECT id FROM groups ORDER BY id DESC LIMIT 1`).Scan(&id)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(id).To(Equal(255))
+
+				tagPopulator.PopulateTables(2)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = realDb.QueryRow(`SELECT id FROM groups ORDER BY id DESC LIMIT 1`).Scan(&id)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(id).To(Equal(255))
+			})
+		})
+	})
+
+	Context("when the groups table fails to populate", func() {
+		var (
+			mockDb *fakes.Db
+		)
+
+		BeforeEach(func() {
+			mockDb = &fakes.Db{}
+
+			mockDb.ExecStub = func(sql string, t ...interface{}) (sql.Result, error) {
+				if strings.Contains(sql, "INSERT") {
+					return nil, errors.New("some error")
+				}
+				return nil, nil
+			}
+
+			tagPopulator = &store.TagPopulator{
+				DBConnection: mockDb,
+			}
+		})
+
+		It("returns an error", func() {
+			err := tagPopulator.PopulateTables(1)
+			Expect(err).To(MatchError("populating tables: some error"))
+		})
+	})
+
+})
