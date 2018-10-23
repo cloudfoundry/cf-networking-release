879e9a80ddbecd106358b460c8d06cbd70642932
Christian Ang <cang@pivotal.io>
2018-10-23 10:25:57 -0700

    Set internal_service_mesh_domains
    in bosh-dns-adapter config
    
    [#161028249]
    
    Signed-off-by: Tyler Schultz <tschultz@pivotal.io>

--- spec/bosh-dns-adapter/bosh_dns_adapter_spec.rb
+++ spec/bosh-dns-adapter/bosh_dns_adapter_spec.rb
@@ -21,6 +21,56 @@ module Bosh::Template::Test
       }
     end
 
+    describe 'config.json' do
+      let(:template) {job.template('config/config.json')}
+      let(:links) do
+          [
+            Link.new(
+              name: 'service-discovery-controller',
+              properties: {
+                'port' => 1234
+              }
+            )
+          ]
+      end
+      it 'renders a file with default properties' do
+        config = JSON.parse(template.render({}, consumes: links))
+        expect(config).to eq({
+          "address" => "127.0.0.1",
+          "ca_cert" => "/var/vcap/jobs/bosh-dns-adapter/config/certs/server_ca.crt",
+          "client_cert" => "/var/vcap/jobs/bosh-dns-adapter/config/certs/client.crt",
+          "client_key" => "/var/vcap/jobs/bosh-dns-adapter/config/certs/client.key",
+          "log_level_address" => "127.0.0.1",
+          "log_level_port" => 8066,
+          "metrics_emit_seconds" => 10,
+          "metron_port" => 3457,
+          "port" => "8053",
+          "service_discovery_controller_address" => "service-discovery-controller.service.cf.internal",
+          "service_discovery_controller_port" => "1234",
+          "internal_service_mesh_domains" => []
+        })
+      end
+      describe 'with custom properties' do
+        it 'renders a file with custom properties' do
+        config = JSON.parse(template.render(merged_manifest_properties, consumes: links))
+        expect(config).to eq({
+          "address" => "127.0.0.1",
+          "ca_cert" => "/var/vcap/jobs/bosh-dns-adapter/config/certs/server_ca.crt",
+          "client_cert" => "/var/vcap/jobs/bosh-dns-adapter/config/certs/client.crt",
+          "client_key" => "/var/vcap/jobs/bosh-dns-adapter/config/certs/client.key",
+          "log_level_address" => "127.0.0.1",
+          "log_level_port" => 8066,
+          "metrics_emit_seconds" => 10,
+          "metron_port" => 3457,
+          "port" => "8053",
+          "service_discovery_controller_address" => "service-discovery-controller.service.cf.internal",
+          "service_discovery_controller_port" => "1234",
+          "internal_service_mesh_domains" => ['myistio.internal.app.domain.']
+        })
+        end
+      end
+    end
+
     describe 'handlers.json' do
       let(:template) {job.template('dns/handlers.json')}
 
