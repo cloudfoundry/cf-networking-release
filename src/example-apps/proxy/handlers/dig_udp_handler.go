9a0cc5310e0dc506788649c32c1ba97cc293d846
Amelia Downs <adowns@pivotal.io>
2018-12-18 09:25:07 -0800

    Add udp only dig handler to proxy app
    [#161904345]
    
    Signed-off-by: Christian Ang <cang@pivotal.io>

---
+++ src/example-apps/proxy/handlers/dig_udp_handler.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+type DigUDPHandler struct {
+}
+
+func (h *DigUDPHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	destination := strings.TrimPrefix(req.URL.Path, "/digudp/")
+	destination = strings.Split(destination, ":")[0]
+
+	cmd := exec.Command("dig", destination, "+short", "+notcp")
+	output, err := cmd.Output()
+
+	if err != nil {
+		handleDigError(err, destination, resp)
+		return
+	}
+
+	ips := strings.Split(string(output), "\n")
+	ips = ips[0 : len(ips)-1]
+
+	ipJson, err := json.Marshal(ips)
+	if err != nil {
+		handleDigError(err, destination, resp)
+		return
+	}
+
+	if len(ips) == 0 {
+		handleDigError(errors.New("no ips found"), destination, resp)
+		return
+	}
+
+	resp.Write(ipJson)
+}
