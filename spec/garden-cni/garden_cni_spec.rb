5f827dfcce01919a87fdcffe20d9984912afe3fb
Tyler Schultz <tschultz@pivotal.io>
2019-01-10 15:35:09 -0800

    garden-cni job consumes cc link for vip range
    [#162564032]
    
    Signed-off-by: Alex Standke <astandke@pivotal.io>

--- spec/garden-cni/garden_cni_spec.rb
+++ spec/garden-cni/garden_cni_spec.rb
@@ -8,6 +8,16 @@ module Bosh::Template::Test
     let(:release_path) {File.join(File.dirname(__FILE__), '../..')}
     let(:release) {ReleaseDir.new(release_path)}
     let(:job) {release.job('garden-cni')}
+    let(:links) do
+      [
+         Link.new(
+           name: 'cloud_controller_container_networking_info',
+           properties: {
+             'cc' => {'internal_route_vip_range' => '192.168.0.1/24'}
+           }
+         )
+      ]
+    end
 
     describe 'adapter.json.erb' do
       let(:template) {job.template('config/adapter.json')}
@@ -21,12 +31,13 @@ module Bosh::Template::Test
             'nat_port_range_size' => 5555,
             'search_domains' => ['meow', 'woof', 'neopets'],
             'experimental_proxy_redirect_cidr' => 'some-proxy-cidr',
+            'experimental_enable_proxy_redirect' => true,
             'experimental_enable_ingress_proxy_redirect' => true
           }
         end
 
         it 'creates a config/adapter.json from properties' do
-          clientConfig = JSON.parse(template.render(merged_manifest_properties))
+          clientConfig = JSON.parse(template.render(merged_manifest_properties, consumes: links))
           expect(clientConfig).to eq({
             'cni_plugin_dir' => 'meow-plugin-dir',
             'cni_config_dir' => 'meow-config-dir',
@@ -38,6 +49,80 @@ module Bosh::Template::Test
             'search_domains' => ['meow', 'woof', 'neopets'],
             'iptables_lock_file' => '/var/vcap/data/garden-cni/iptables.lock',
             'proxy_redirect_cidr' => 'some-proxy-cidr',
+            'proxy_port' => 15001,
+            'proxy_uid' => 0,
+            'enable_ingress_proxy_redirect' => true,
+          })
+        end
+      end
+     
+      describe 'when accepting the value from the link' do
+        let(:merged_manifest_properties) do
+          {
+            'cni_plugin_dir' => 'meow-plugin-dir',
+            'cni_config_dir' => 'meow-config-dir',
+            'nat_port_range_start' => 1111,
+            'nat_port_range_size' => 5555,
+            'search_domains' => ['meow', 'woof', 'neopets'],
+            'experimental_enable_proxy_redirect' => true,
+            'experimental_enable_ingress_proxy_redirect' => true
+          }
+        end
+
+        it 'uses the value from the link' do
+          clientConfig = JSON.parse(template.render(merged_manifest_properties, consumes: links))
+          expect(clientConfig['proxy_redirect_cidr']).to eq('192.168.0.1/24')
+        end
+      end
+
+      describe 'when the link is not present' do
+        let(:merged_manifest_properties) do
+          {
+            'cni_plugin_dir' => 'meow-plugin-dir',
+            'cni_config_dir' => 'meow-config-dir',
+            'nat_port_range_start' => 1111,
+            'nat_port_range_size' => 5555,
+            'search_domains' => ['meow', 'woof', 'neopets'],
+            'experimental_proxy_redirect_cidr' => nil,
+            'experimental_enable_proxy_redirect' => true,
+            'experimental_enable_ingress_proxy_redirect' => true
+          }
+        end
+
+        it 'renders empty string' do
+          clientConfig = JSON.parse(template.render(merged_manifest_properties))
+          expect(clientConfig['proxy_redirect_cidr']).to eq('')
+        end
+      end
+
+
+      describe 'when enable proxy redirect is false' do
+        let(:merged_manifest_properties) do
+          {
+            'cni_plugin_dir' => 'meow-plugin-dir',
+            'cni_config_dir' => 'meow-config-dir',
+            'nat_port_range_start' => 1111,
+            'nat_port_range_size' => 5555,
+            'search_domains' => ['meow', 'woof', 'neopets'],
+            'experimental_proxy_redirect_cidr' => 'some-proxy-cidr',
+            'experimental_enable_proxy_redirect' => false,
+            'experimental_enable_ingress_proxy_redirect' => true
+          }
+        end
+
+        it 'creates a config/adapter.json from properties' do
+          clientConfig = JSON.parse(template.render(merged_manifest_properties), consumes: links)
+          expect(clientConfig).to eq({
+            'cni_plugin_dir' => 'meow-plugin-dir',
+            'cni_config_dir' => 'meow-config-dir',
+            'bind_mount_dir' => '/var/vcap/data/garden-cni/container-netns',
+            'state_file' => '/var/vcap/data/garden-cni/external-networker-state.json',
+            'start_port' => 1111,
+            'total_ports' => 5555,
+            'log_prefix' => 'cfnetworking',
+            'search_domains' => ['meow', 'woof', 'neopets'],
+            'iptables_lock_file' => '/var/vcap/data/garden-cni/iptables.lock',
+            'proxy_redirect_cidr' => '',
             'proxy_port' => 15001,
             'proxy_uid' => 0,
             'enable_ingress_proxy_redirect' => true,
