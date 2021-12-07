4313c84c538da8fbd9c9302d4a4b0393b7cc3b70
Renee Chu <reneec@vmware.com>
2021-12-07 23:15:02 +0000

    Add `IF EXISTS` conditional to `DROP PROCEDURE` statement
    * Running v0066 was causing errors if the stored procedure was already
    absent from the database.
    * This may occur if the customer deleted the stored procedure out of
      band, or if they upgraded to v0066, then downgraded and and upgraded
      and ran v0066 again.
    
    [#180426713](https://www.pivotaltracker.com/story/show/180426713)

--- src/code.cloudfoundry.org/policy-server/store/migrations/v0066.go
+++ src/code.cloudfoundry.org/policy-server/store/migrations/v0066.go
@@ -2,7 +2,7 @@ package migrations
 
 var migration_v0066 = map[string][]string{
 	"mysql": {
-		`DROP PROCEDURE drop_destination_index`,
+		`DROP PROCEDURE IF EXISTS drop_destination_index`,
 	},
 	"postgres": {},
 }
