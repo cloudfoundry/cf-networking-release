81e6bf17feead6fd9e1ee83507c037413a022fa7
Maria Shaldybin <mariash@vmware.com>
2022-01-06 23:48:37 +0000

    Convert security space groups to database json type
    - Security groups fetch works on MySQL

--- src/code.cloudfoundry.org/policy-server/store/helpers/helpers.go
+++ src/code.cloudfoundry.org/policy-server/store/helpers/helpers.go
@@ -11,10 +11,14 @@ const (
 )
 
 func QuestionMarks(count int) string {
+	return MarksWithSeparator(count, "?", ", ")
+}
+
+func MarksWithSeparator(count int, mark string, separator string) string {
 	if count == 0 {
 		return ""
 	}
-	return strings.Repeat("?, ", count-1) + "?"
+	return strings.Repeat(mark+separator, count-1) + mark
 }
 
 func RebindForSQLDialect(query, dialect string) string {
@@ -31,3 +35,20 @@ func RebindForSQLDialect(query, dialect string) string {
 	}
 	return strings.Join(strParts, "")
 }
+
+func RebindForSQLDialectAndMark(query, dialect, mark string) string {
+	if dialect != Postgres && dialect != MySQL {
+		panic(fmt.Sprintf("Unrecognized DB dialect '%s'", dialect))
+	}
+
+	if dialect == MySQL {
+		return strings.ReplaceAll(query, mark, "?")
+	}
+
+	strParts := strings.Split(query, mark)
+	for i := 1; i < len(strParts); i++ {
+		strParts[i-1] = fmt.Sprintf("%s$%d", strParts[i-1], i)
+	}
+	return strings.Join(strParts, "")
+}
+
--- src/code.cloudfoundry.org/policy-server/store/security_groups.go
+++ src/code.cloudfoundry.org/policy-server/store/security_groups.go
@@ -1,13 +1,39 @@
 package store
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
+
 type SecurityGroup struct {
 	Guid              string
 	Name              string
 	Rules             string
 	StagingDefault    bool
 	RunningDefault    bool
-	StagingSpaceGuids []string
-	RunningSpaceGuids []string
+	StagingSpaceGuids SpaceGuids
+	RunningSpaceGuids SpaceGuids
+}
+
+type SpaceGuids []string
+
+func (guids SpaceGuids) Value() (driver.Value, error) {
+	return json.Marshal(guids)
+}
+
+func (guids *SpaceGuids) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	err := json.Unmarshal(b, &guids)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 type Page struct {
@@ -17,5 +43,4 @@ type Page struct {
 
 type Pagination struct {
 	Next int
-	Prev int
 }
