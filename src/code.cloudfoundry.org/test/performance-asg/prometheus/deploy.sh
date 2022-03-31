71dd284b5ea40ccc8b59d42f7a960bd8e9b57e0e
Geoff Franks <gfranks@vmware.com>
2022-03-31 14:48:11 +0000

    update prometheus deploy script to work with cf-d bosh envs

--- src/code.cloudfoundry.org/test/performance-asg/prometheus/deploy.sh
+++ src/code.cloudfoundry.org/test/performance-asg/prometheus/deploy.sh
@@ -6,6 +6,11 @@ set -eu
 BOSH_ENVIRONMENT=${BOSH_ENVIRONMENT:?must set BOSH_ENVIRONMENT env var}
 BOSH_CA_CERT=${BOSH_CA_CERT:?must set BOSH_CA_CERT env var}
 
+BOSH_CA_ARG="-v"
+if ! echo "${BOSH_CA_CERT}" | grep "END CERTIFICATE" >& /dev/null; then
+  BOSH_CA_ARG="--var-file"
+fi
+
 PROMETHEUS_BOSH_RELEASE_DIR=${PROMETHEUS_BOSH_RELEASE_DIR:=~/workspace/prometheus-boshrelease}
 ASG_TEST_DIR=${ASG_TEST_DIR:=~/workspace/cf-networking-release/src/code.cloudfoundry.org/test/performance-asg/prometheus}
 
@@ -13,7 +18,7 @@ bosh -d prometheus deploy "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/prometheus.y
   --vars-store "${ASG_TEST_DIR}/change-me-deployment-vars.yml" \
   -o "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/operators/monitor-bosh.yml" \
   -v bosh_url="${BOSH_ENVIRONMENT}" \
-  --var-file bosh_ca_cert="${BOSH_CA_CERT}" \
+  "${BOSH_CA_ARG}" "bosh_ca_cert=${BOSH_CA_CERT}" \
   -o "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/operators/monitor-cf.yml" \
   -o "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/operators/enable-cf-route-registrar.yml" \
   -o "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/operators/enable-cf-api-v3.yml" \
