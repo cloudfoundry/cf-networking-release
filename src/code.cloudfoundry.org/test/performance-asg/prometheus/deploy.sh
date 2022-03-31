bf081cb69f5661ea247aae9bbe11f6273d5e09dd
Geoff Franks <gfranks@vmware.com>
2022-03-31 14:49:37 +0000

    use bosh client-id/secret for bosh_exporter in performance-asg prometheus env

--- src/code.cloudfoundry.org/test/performance-asg/prometheus/deploy.sh
+++ src/code.cloudfoundry.org/test/performance-asg/prometheus/deploy.sh
@@ -17,11 +17,13 @@ ASG_TEST_DIR=${ASG_TEST_DIR:=~/workspace/cf-networking-release/src/code.cloudfou
 bosh -d prometheus deploy "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/prometheus.yml" \
   --vars-store "${ASG_TEST_DIR}/change-me-deployment-vars.yml" \
   -o "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/operators/monitor-bosh.yml" \
-  -v bosh_url="${BOSH_ENVIRONMENT}" \
+  -v bosh_url="${BOSH_ENVIRONMENT}" -v "uaa_bosh_exporter_client_id=${BOSH_CLIENT}" \
+  -v "uaa_bosh_exporter_client_secret=${BOSH_CLIENT_SECRET}" \
   "${BOSH_CA_ARG}" "bosh_ca_cert=${BOSH_CA_CERT}" \
   -o "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/operators/monitor-cf.yml" \
   -o "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/operators/enable-cf-route-registrar.yml" \
   -o "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/operators/enable-cf-api-v3.yml" \
   -o "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/operators/enable-cf-loggregator-v2.yml" \
+  -o "${PROMETHEUS_BOSH_RELEASE_DIR}/manifests/operators/enable-bosh-uaa.yml" \
   -o "${ASG_TEST_DIR}/change-prometheus-defaults.yml" \
   "$@"
