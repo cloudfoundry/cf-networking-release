e33447b84a94ddc80845c84903c9371eda6cbc74
Marc Paquette <mpaquette@vmware.com>
2022-04-07 20:06:51 +0000

    Add Database migrations to remove Dynamic egress tables

---
+++ src/code.cloudfoundry.org/policy-server/store/migrations/v0074.go
@@ -0,0 +1,14 @@
+package migrations
+
+// Dropping Dynamic Egress tables
+// There are 7 tables related to dynamic egress policies:
+// apps, defaults, destination_metadatas, egress_policies, ip_ranges, terminals, and spaces.
+
+var migration_v0074 = map[string][]string{
+	"mysql": []string{
+		`DROP TABLE IF EXISTS terminals;`,
+	},
+	"postgres": []string{
+		`DROP TABLE IF EXISTS terminals;`,
+	},
+}
