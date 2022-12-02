1c0dfd9fe2cd69667e477c391707a5eb6283b6fc
Geoff Franks <gfranks@vmware.com>
2022-12-02 18:58:20 +0000

    Update bosh-dns-adapter to use failure counter file
    Signed-off-by: Josh Russett <jrussett@vmware.com>

--- src/code.cloudfoundry.org/vendor/code.cloudfoundry.org/cf-networking-helpers/healthchecker/watchdog/watchdog.go
+++ src/code.cloudfoundry.org/vendor/code.cloudfoundry.org/cf-networking-helpers/healthchecker/watchdog/watchdog.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -17,23 +21,41 @@ const (
 )
 
 type Watchdog struct {
-	host          string
-	componentName string
-	pollInterval  time.Duration
-	client        http.Client
-	logger        lager.Logger
+	url            *url.URL
+	componentName  string
+	pollInterval   time.Duration
+	client         http.Client
+	logger         lager.Logger
+	failureCounter *FailureCounter
 }
 
-func NewWatchdog(host string, componentName string, pollInterval time.Duration, healthcheckTimeout time.Duration, logger lager.Logger) *Watchdog {
+func NewWatchdog(u *url.URL, componentName string, failureCounterFileName string, pollInterval time.Duration, healthcheckTimeout time.Duration, logger lager.Logger) *Watchdog {
 	client := http.Client{
 		Timeout: healthcheckTimeout,
 	}
+	if strings.HasPrefix(u.Host, "unix") {
+		socket := strings.TrimPrefix(u.Host, "unix")
+		client.Transport = &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", socket)
+			},
+		}
+	}
+
+	failureCounterFile, err := os.OpenFile(failureCounterFileName, os.O_RDONLY|os.O_CREATE, 0644)
+	if err != nil {
+		logger.Fatal("cannot-create-failure-counter-file", err)
+		return nil
+	}
+	failureCounterFile.Close()
+
 	return &Watchdog{
-		host:          host,
-		componentName: componentName,
-		pollInterval:  pollInterval,
-		client:        client,
-		logger:        logger,
+		url:            u,
+		componentName:  componentName,
+		pollInterval:   pollInterval,
+		client:         client,
+		logger:         logger,
+		failureCounter: &FailureCounter{file: failureCounterFileName, logger: logger},
 	}
 }
 
@@ -64,12 +86,20 @@ func (w *Watchdog) WatchHealthcheckEndpoint(ctx context.Context, signals <-chan 
 							return nil
 						}
 					default:
+						incrErr := w.failureCounter.Increment()
+						if incrErr != nil {
+							w.logger.Error("incrementing-failure-count", incrErr, lager.Data{"file": w.failureCounter.file})
+						}
 						return err
 					}
 				} else {
 					w.logger.Debug("Received error", lager.Data{"error": err.Error(), "attempt": errCounter})
 				}
 			} else {
+				err = w.failureCounter.Set(0)
+				if err != nil {
+					w.logger.Error("resetting-failure-count", err, lager.Data{"file": w.failureCounter.file})
+				}
 				errCounter = 0
 			}
 			pollTimer.Reset(w.pollInterval)
@@ -78,15 +108,48 @@ func (w *Watchdog) WatchHealthcheckEndpoint(ctx context.Context, signals <-chan 
 }
 
 func (w *Watchdog) HitHealthcheckEndpoint() error {
-	response, err := w.client.Get(w.host)
+	req, err := http.NewRequest("GET", w.url.String(), nil)
 	if err != nil {
 		return err
 	}
-	// fmt.Printf("status: %d", response.StatusCode)
+	if req.URL.Host == "" {
+		req.URL.Host = w.url.Host
+	}
+
+	response, err := w.client.Do(req)
+	if err != nil {
+		return err
+	}
+
 	if response.StatusCode != http.StatusOK {
 		return errors.New(fmt.Sprintf(
 			"%v received from healthcheck endpoint (200 expected)",
 			response.StatusCode))
 	}
 	return nil
+}
+
+type FailureCounter struct {
+	file   string
+	logger lager.Logger
+}
+
+func (fc *FailureCounter) Set(i int) error {
+	return os.WriteFile(fc.file, []byte(fmt.Sprintf("%d\n", i)), 0644)
+}
+
+func (fc *FailureCounter) Increment() error {
+	failures, err := os.ReadFile(fc.file)
+	if err != nil {
+		return err
+	}
+
+	failuresInt, err := strconv.Atoi(strings.TrimSpace(string(failures)))
+	if err != nil {
+		fc.logger.Info("converting-failure-counter-string-to-int", lager.Data{"error": err, "message": "Resetting counter to 0"})
+		failuresInt = 0
+	}
+
+	failuresInt++
+	return fc.Set(failuresInt)
 }
