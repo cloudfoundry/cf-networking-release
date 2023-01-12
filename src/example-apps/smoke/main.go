cc60e774fe3a0ec24e9d406ad528a9178842577f
Renee Chu <reneec@vmware.com>
2023-01-12 23:05:01 +0000

    Revert "Removing smoke app since it isn't used any more"
    This reverts commit 25b1effc5fefb57ae19da1a45e42aca85c2fbb05.

---
+++ src/example-apps/smoke/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+func launchServer(port int) {
+	helloHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello"))
+	})
+
+	internalAddressString := os.Getenv("CF_INSTANCE_INTERNAL_IP")
+	internalAddress := net.ParseIP(internalAddressString)
+	if internalAddress == nil {
+		log.Fatal("invalid required env var CF_INSTANCE_INTERNAL_IP")
+	}
+
+	proxyURL := os.Getenv("PROXY_APP_URL")
+	if proxyURL == "" {
+		log.Fatal("invalid required env var PROXY_APP_URL")
+	}
+
+	selfProxyHandler := &SelfProxyHandler{
+		SelfAddress: internalAddress.String(),
+		ProxyURL:    proxyURL,
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/selfproxy", selfProxyHandler)
+	mux.Handle("/", helloHandler)
+	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), mux)
+}
+
+func main() {
+	systemPortString := os.Getenv("PORT")
+	systemPort, err := strconv.Atoi(systemPortString)
+	if err != nil {
+		log.Fatal("invalid required env var PORT")
+	}
+
+	launchServer(systemPort)
+}
+
+type SelfProxyHandler struct {
+	SelfAddress string
+	ProxyURL    string
+}
+
+var httpClient = &http.Client{
+	Transport: &http.Transport{
+		DisableKeepAlives: true,
+		Dial: (&net.Dialer{
+			Timeout:   4 * time.Second,
+			KeepAlive: 0,
+		}).Dial,
+	},
+}
+
+func (h *SelfProxyHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	newURL := fmt.Sprintf("%s/proxy/%s:8080", h.ProxyURL, h.SelfAddress)
+	getResp, err := httpClient.Get(newURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "request failed: %s", err)
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(fmt.Sprintf("request failed: %s", err)))
+		return
+	}
+	defer getResp.Body.Close()
+
+	resp.WriteHeader(getResp.StatusCode)
+	switch getResp.StatusCode {
+	case http.StatusOK:
+		resp.Write([]byte("OK"))
+	default:
+		resp.Write([]byte("FAILED"))
+	}
+}
