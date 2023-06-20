993ec4b565bbaff98abc25ca79d7a8e918c906df
Pascal Zimmermann <pascal.zimmermann@theiotstudio.com>
2023-06-20 16:56:25 +0200

    fix: Change the update command

--- src/code.cloudfoundry.org/policy-server/store/migrations/v0075.go
+++ src/code.cloudfoundry.org/policy-server/store/migrations/v0075.go
@@ -2,14 +2,10 @@ package migrations
 
 var migration_v0075 = map[string][]string{
 	"mysql": []string{
-		`ALTER TABLE security_groups DROP PRIMARY KEY;`,
 		`ALTER TABLE security_groups MODIFY COLUMN id bigint;`,
-		`ALTER TABLE security_groups ADD PRIMARY KEY (id);`,
 	},
 	"postgres": []string{
-		`ALTER TABLE security_groups DROP CONSTRAINT security_groups_pkey;`,
 		`ALTER TABLE security_groups ALTER COLUMN id type bigint;`,
-		`ALTER TABLE security_groups ADD PRIMARY KEY (id);`,
 		`ALTER SEQUENCE public.security_groups_id_seq as bigint MAXVALUE 9223372036854775807;`,
 	},
 }
