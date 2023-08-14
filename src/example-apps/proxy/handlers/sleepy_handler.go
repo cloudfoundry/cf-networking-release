5cd835a2b30ef709ad22bcb34e2cf1a7bd493bb4
Renee Chu <reneec@vmware.com>
2023-08-14 23:58:25 +0000

    add test for endpoint that sleeps before responding 200, to test readines invocation timeout
    Signed-off-by: Marc Paquette <mpaquette@vmware.com>

---
+++ src/example-apps/proxy/handlers/sleepy_handler.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"net/http"
+	"time"
+)
+
+type SleepyHandler struct {
+	Port           int
+	SleepyInterval int
+}
+
+func (h *SleepyHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	time.Sleep(time.Duration(h.SleepyInterval) * time.Second)
+
+	respBytes := []byte("😴")
+	resp.Write(respBytes)
+	return
+}
