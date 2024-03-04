05e5e36d9241b1e4e0a413075df44f5618488d2c
Geoff Franks <geoff.franks@broadcom.com>
2024-03-04 15:57:19 +0000

    Upate local-testing scripts to add starting into docker locally"

---
+++ scripts/docker/init.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for dir in /built-binaries/*; do
+  envVar="$(echo "${dir}" | cut -d "/" -f 3 | tr '[:lower:]' '[:upper:]' | tr '-' '_')_BINARY"
+  eval "export ${envVar}=$dir/run";
+done
+
+pushd /repo
+. "/ci/shared/helpers/helpers.bash"
+expand_functions
+
+configure_db "${DB}"
