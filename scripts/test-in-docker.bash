264ce9ff691c883c4f7b95eff44e8b84ef8cfcb3
Amin Jamali <71907054+winkingturtle-vmw@users.noreply.github.com>
2024-04-08 14:36:55 +0000

    Update scripts dir (#278)

--- scripts/test-in-docker.bash
+++ scripts/test-in-docker.bash
@@ -3,6 +3,17 @@
 set -eu
 
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-. "${THIS_FILE_DIR}/local-testing-helper.sh"
+CI="${THIS_FILE_DIR}/../../wg-app-platform-runtime-ci"
+. "$CI/shared/helpers/git-helpers.bash"
+REPO_NAME=$(git_get_remote_name)
 
-test_in_docker_locally "$@"
+if [[ ${DB:-empty} == "empty" ]]; then
+  DB=mysql
+fi
+CONTAINER_NAME="$REPO_NAME-$DB-docker-container"
+
+DB="${DB}" "${THIS_FILE_DIR}/create-docker-container.bash" -d
+
+docker exec $CONTAINER_NAME '/repo/scripts/docker/tests-templates.bash'
+docker exec $CONTAINER_NAME '/repo/scripts/docker/test.bash' "$@"
+docker exec $CONTAINER_NAME '/repo/scripts/docker/lint.bash'
