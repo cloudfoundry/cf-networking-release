7e8ea23eff1e65875fdd4eaae98a0c5c5ebd6ec4
Geoff Franks <geoff.franks@broadcom.com>
2025-05-07 22:20:24 +0000

    Add migrations to rollback the bad v0082 and v0083 migrations
    Signed-off-by: Maria Shaldybin <maria.shaldybin@broadcom.com>

--- src/code.cloudfoundry.org/policy-server/store/migrations/v0082.go
+++ src/code.cloudfoundry.org/policy-server/store/migrations/v0082.go
@@ -1,8 +1,8 @@
 package migrations
 
 var migration_v0082 = map[string][]string{
-	"mysql": {
-		`CREATE INDEX staging_spaces_idx ON security_groups ((CAST(staging_spaces -> '$[*]' AS CHAR(36) ARRAY)))`,
-	},
+	"mysql": {},
+	// This ran into issues with index limits when ASGs are bound to > 148 spaces. Rolling it back.
+	//		`CREATE INDEX staging_spaces_idx ON security_groups ((CAST(staging_spaces -> '$[*]' AS CHAR(36) ARRAY)))`,
 	"postgres": {},
 }
--- src/code.cloudfoundry.org/policy-server/store/migrations/v0083.go
+++ src/code.cloudfoundry.org/policy-server/store/migrations/v0083.go
@@ -1,8 +1,8 @@
 package migrations
 
 var migration_v0083 = map[string][]string{
-	"mysql": {
-		`CREATE INDEX running_spaces_idx ON security_groups ((CAST(running_spaces -> '$[*]' AS CHAR(36) ARRAY)))`,
-	},
+	"mysql": {},
+	// This ran into issues with index limits when ASGs are bound to > 148 spaces. Rolling it back.
+	//	`CREATE INDEX running_spaces_idx ON security_groups ((CAST(running_spaces -> '$[*]' AS CHAR(36) ARRAY)))`,
 	"postgres": {},
 }
