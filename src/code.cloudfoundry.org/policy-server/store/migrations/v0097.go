d00b92b6360a8b1b4f5f00c90bf4e8b3a5aa7c6d
Geoff Franks <geoff.franks@broadcom.com>
2025-09-03 14:45:22 +0000

    Update migrations 97 and 98 to be more tolerant of environments where spaces have somehow been bound multiple times to security groups

--- src/code.cloudfoundry.org/policy-server/store/migrations/v0097.go
+++ src/code.cloudfoundry.org/policy-server/store/migrations/v0097.go
@@ -9,12 +9,12 @@ var migration_v0097 = map[string][]string{
 			security_groups
 		JOIN 
 			(SELECT id from temp_sequence) x
-		WHERE x.id < JSON_LENGTH(security_groups.running_spaces)`,
+		WHERE x.id < JSON_LENGTH(security_groups.running_spaces) ON DUPLICATE KEY UPDATE space_guid = VALUES(space_guid), security_group_guid = VALUES(security_group_guid)`,
 	},
 	"postgres": {
 		`INSERT INTO running_security_groups_spaces (security_group_guid, space_guid) SELECT
 			guid AS security_group_guid,
 			jsonb_array_elements_text(running_spaces) AS space_id
-		FROM security_groups`,
+		FROM security_groups ON CONFLICT (security_group_guid, space_guid) DO NOTHING`,
 	},
 }
